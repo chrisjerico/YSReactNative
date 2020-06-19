4903c1d0c52595bf2f15f2dd7eb048b5aa5b8567
Ezer <ugrnezer@gmail.com>
2020-06-19 17:53:41 +0800

    remove transfer UI

--- js/rn/pages/router/UGApplication.tsx
+++ js/rn/pages/router/UGApplication.tsx
@@ -4,34 +4,26 @@ import * as React from 'react';
 import {Provider} from 'react-redux';
 import {PageName} from '../../public/navigation/Navigation';
 import {Router} from '../../public/navigation/Router';
-import {updateUserInfo} from '../../redux/store/IGlobalStateHelper';
+import {IGlobalStateHelper} from '../../redux/store/IGlobalStateHelper';
 import {UGStore} from '../../redux/store/UGStore';
 import TransitionPage from '../base/TransitionPage';
-import LHTHomePage from '../六合厅/LHTHomePage';
-import LHTMinePage from '../六合厅/LHTMinePage';
-import BZHHomePage from '../宝石红/BZHHomePage';
-import BZHMinePage from '../宝石红/BZHMinePage';
 import JDPromotionListPage from '../经典/JDPromotionListPage';
 import {NavigationContainer} from '@react-navigation/native';
 import ZHTYLoginPage from '../综合体育/ZHTYLoginPage';
 import ZHTYRegisterPage from '../综合体育/ZHTYRegisterPage';
 import ZHTYMinePage from '../综合体育/ZHTYMinePage';
-import ZHTYHomePage from '../综合体育/ZHTYHomePage';
 import LCHomePage from "../乐橙/LCHomePage";
 import ZLHomePage from '../尊龙/ZLHomePage';
 import ZLLoginPage from '../尊龙/ZLLoginPage';
-import AppDefine from '../../public/define/AppDefine';
 import {navigationRef} from '../../public/navigation/RootNavigation';
 import ZLHomeMine from '../尊龙/ZLHomeMine';
 import ZLRegisterPage from '../尊龙/ZLRegisterPage';
-import {IGlobalStateHelper} from '../../redux/store/IGlobalStateHelper';
 import LCMinePage from "../乐橙/LCMinePage";
 import XBJHomePage from '../香槟金/XBJHomePage';
 import XBJLoginPage from '../香槟金/XBJLoginPage';
 import XBJMinePage from '../香槟金/XBJMinePage';
 import XBJRegisterPage from '../香槟金/XBJRegisterPage';
 import UpdateVersionPage from './UpdateVersionPage';
-import LCTransferPage from "../乐橙/LCTransferPage";
 import LXBView from "../乐橙/component/minePage/LXBView";
 
 // TabbarController
@@ -53,7 +45,6 @@ class TabBarController extends React.Component<{ navigation: StackNavigationProp
             <Router.TabNavigator initialRouteName={PageName.UpdateVersionPage} screenOptions={{tabBarVisible: false}}
                                  tabBarOptions={this.tabBarOptions}>
                 <Router.TabScreen name={PageName.LXBView} component={LXBView} />
-                <Router.TabScreen name={PageName.LCTransferPage} component={LCTransferPage} />
                 <Router.TabScreen name={PageName.LCMinePage} component={LCMinePage}/>
                 <Router.TabScreen name={PageName.LCHomePage} component={LCHomePage}/>
                 <Router.TabScreen name={PageName.UpdateVersionPage} component={UpdateVersionPage}/>
