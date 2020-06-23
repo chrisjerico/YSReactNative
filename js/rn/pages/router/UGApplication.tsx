404320f3d0a20679878c835110804acee16490d4
Arc <arclinde@gmail.com>
2020-06-23 14:17:48 +0800

    调试 LCHomePage

--- js/rn/pages/router/UGApplication.tsx
+++ js/rn/pages/router/UGApplication.tsx
@@ -53,7 +53,8 @@ class TabBarController extends Component<{
 
     render() {
         return (
-            <Router.TabNavigator initialRouteName={PageName.ZLHomePage} >
+          <Router.TabNavigator initialRouteName={PageName.UpdateVersionPage} screenOptions={{tabBarVisible: false}}
+                               tabBarOptions={this.tabBarOptions}>
                 <Router.TabScreen name={PageName.LXBView} component={LXBView} />
                 <Router.TabScreen name={PageName.LCMinePage} component={LCMinePage}/>
                 <Router.TabScreen name={PageName.LCHomePage} component={LCHomePage}/>
--- js/rn/public/hooks/useGetHomeInfo.tsx
+++ js/rn/public/hooks/useGetHomeInfo.tsx
@@ -13,6 +13,8 @@ import { RedBagDetailActivityModel } from '../network/Model/RedBagDetailActivity
 import { NoticeModel } from '../network/Model/NoticeModel'
 import { LhcdocCategoryListModel } from '../network/Model/LhcdocCategoryListModel'
 import { TurntableListModel } from '../network/Model/TurntableListModel'
+import {Platform} from "react-native";
+import {ANHelper, NativeCommand} from "../define/ANHelper/ANHelper";
 
 type APIListType =
   | 'game_homeGames'
@@ -41,101 +43,117 @@ const useGetHomeInfo = (coustomArray?: APIListType[]) => {
   const [turntableList, setTurntableList] = useState<TurntableListModel>()
   useEffect(() => {
     init()
-    console.log('render')
+    console.log('useEffect render')
   }, [])
   const init = () => {
-    OCHelper.call('AppDefine.shared.Host').then((host: string) => {
-      httpClient.defaults.baseURL = host
-      if (coustomArray?.length > 0) {
-        let requests = []
-        for (const key in coustomArray) {
-          if (coustomArray.hasOwnProperty(key)) {
-            const element = coustomArray[key]
-            requests.push(APIRouter[element]())
-          }
+    console.log('useEffect init')
+    if (Platform.OS == 'ios') {
+      OCHelper.call('AppDefine.shared.Host').then((host: string) => {
+        _handleHost(host)
+      })
+
+    } else if (Platform.OS == 'android') {
+      ANHelper.call(NativeCommand.APP_HOST).then((host: string) => {
+        _handleHost(host)
+      })
+
+    }
+  }
+
+  const _handleHost = (host?: String) => {
+    console.log('useEffect host=' + host)
+    httpClient.defaults.baseURL = "http://test30f.fhptcdn.com"
+    if (coustomArray?.length > 0) {
+      let requests = []
+      for (const key in coustomArray) {
+        if (coustomArray.hasOwnProperty(key)) {
+          const element = coustomArray[key]
+          requests.push(APIRouter[element]())
         }
-        Axios.all(requests)
-          .then(
-            Axios.spread((...res) => {
-              for (const key in coustomArray) {
-                if (coustomArray.hasOwnProperty(key)) {
-                  const element: APIListType = coustomArray[key]
-                  switch (element) {
-                    case 'game_homeGames':
-                      console.log('----------game_homeGames--------', res[key].data)
-                      setHomeGames(res[key].data)
-                      break
-                    case 'system_banners':
-                      setBanner(res[key].data)
-                      break
-                    case 'notice_latest':
-                      setNotice(res[key].data)
-                      break
-                    case 'system_promotions':
-                      setCouponListData(res[key].data)
-                      break
-                    case 'system_rankingList':
-                      setRankList(res[key].data)
-                      break
-                    case 'activity_redBagDetail':
-                      setRedBag(res[key].data)
-                      break
-                    case 'system_floatAds':
-                      setFloatAds(res[key].data)
-                      break
-                    case 'system_onlineCount':
-                      setOnlineNum(res[key].data.data.onlineUserCount)
-                      break
-                    case 'lhcdoc_lotteryNumber':
-                      setLotteryNumber(res[key].data)
-                      break
-                    case 'lhcdoc_categoryList':
-                      setCategoryList(res[key].data)
-                      break
-                    case 'activity_turntableList':
-                      setTurntableList(res[key].data)
-                    default:
-                      break
-                  }
+      }
+      Axios.all(requests)
+        .then(
+          Axios.spread((...res) => {
+            for (const key in coustomArray) {
+              if (coustomArray.hasOwnProperty(key)) {
+                const element: APIListType = coustomArray[key]
+                switch (element) {
+                  case 'game_homeGames':
+                    console.log('----------game_homeGames--------', res[key].data)
+                    setHomeGames(res[key].data)
+                    break
+                  case 'system_banners':
+                    setBanner(res[key].data)
+                    break
+                  case 'notice_latest':
+                    setNotice(res[key].data)
+                    break
+                  case 'system_promotions':
+                    setCouponListData(res[key].data)
+                    break
+                  case 'system_rankingList':
+                    setRankList(res[key].data)
+                    break
+                  case 'activity_redBagDetail':
+                    setRedBag(res[key].data)
+                    break
+                  case 'system_floatAds':
+                    setFloatAds(res[key].data)
+                    break
+                  case 'system_onlineCount':
+                    setOnlineNum(res[key].data.data.onlineUserCount)
+                    break
+                  case 'lhcdoc_lotteryNumber':
+                    setLotteryNumber(res[key].data)
+                    break
+                  case 'lhcdoc_categoryList':
+                    setCategoryList(res[key].data)
+                    break
+                  case 'activity_turntableList':
+                    setTurntableList(res[key].data)
+                  default:
+                    break
                 }
               }
-              setLoading(false)
-            })
-          )
-          .catch(error => {
-            setLoading(false)
-            console.log(error)
-          })
-      } else {
-        Axios.all([
-          APIRouter.game_homeGames(),
-          APIRouter.system_banners(),
-          APIRouter.notice_latest(),
-          APIRouter.system_promotions(),
-          APIRouter.system_rankingList(),
-          APIRouter.system_onlineCount(),
-          APIRouter.activity_redBagDetail(),
-          APIRouter.system_floatAds(),
-        ])
-          .then(
-            Axios.spread((...res) => {
-              setHomeGames(res[0].data)
-              setBanner(res[1].data)
-              setCouponListData(res[3].data)
-              setRankList(res[4].data)
-              setRedBag(res[6].data)
-              setFloatAds(res[7].data)
-              setNotice(res[2].data)
-              setOnlineNum(res[5].data.data.onlineUserCount)
-              setLoading(false)
-            })
-          )
-          .catch(err => {
+            }
             setLoading(false)
           })
-      }
-    })
+        )
+        .catch(error => {
+          setLoading(false)
+          console.log(error)
+        })
+    } else {
+      Axios.all([
+        APIRouter.game_homeGames(),
+        APIRouter.system_banners(),
+        APIRouter.notice_latest(),
+        APIRouter.system_promotions(),
+        APIRouter.system_rankingList(),
+        APIRouter.system_onlineCount(),
+        APIRouter.activity_redBagDetail(),
+        APIRouter.system_floatAds(),
+      ])
+        .then(
+          Axios.spread((...res) => {
+            setHomeGames(res[0].data)
+            setBanner(res[1].data)
+            setCouponListData(res[3].data)
+            setRankList(res[4].data)
+            setRedBag(res[6].data)
+            setFloatAds(res[7].data)
+            setNotice(res[2].data)
+            setOnlineNum(res[5].data.data.onlineUserCount)
+            setLoading(false)
+          })
+        )
+        .catch(err => {
+          setLoading(false)
+        })
+    }
+
   }
+
   debugger
   return {
     onlineNum,
