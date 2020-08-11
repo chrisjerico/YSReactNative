e48b305ffc65ddbcac1b40d4f2478586b399e0ec
Ezer <ugrnezer@gmail.com>
2020-08-11 17:03:36 +0800

    fix UGSkinManagers

--- js/rn/public/theme/UGSkinManagers.tsx
+++ js/rn/public/theme/UGSkinManagers.tsx
@@ -62,7 +62,7 @@ export default class UGSkinManagers extends UGThemeColor {
       24: `白曜`,
     };
     console.log('pi fu =', mobileTemplateCategory);
-    let key = dict[mobileTemplateStyle];
+    let key = dict[mobileTemplateCategory];
     let theme = { ...new UGThemeColor(), ...this.allThemeColor[key] };
     theme.themeColor = theme.themeColor ?? chroma.scale(theme.navBarBgColor)(0.5).hex();
     theme.themeDarkColor = theme.themeDarkColor ?? chroma(theme.themeColor).darken().hex();
