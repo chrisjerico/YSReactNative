5f9ea1bd5aae3acd5c07be50660c831b6c7e3218
Arc <arclinde@gmail.com>
2020-08-19 20:50:11 +0800

    退出登录处理

--- js/rn/public/hooks/useLoginOut.tsx
+++ js/rn/public/hooks/useLoginOut.tsx
@@ -5,22 +5,28 @@ import { PageName } from '../navigation/Navigation'
 import { navigate } from '../navigation/RootNavigation'
 import APIRouter from '../network/APIRouter'
 import { Toast } from '../tools/ToastUtils'
+import {ANHelper, CMD, NA_DATA} from "../define/ANHelper/ANHelper";
 
 const useLoginOut = (pageName: PageName) => {
   const requestLoginOut = async () => {
     try {
       await APIRouter.user_logout()
-      if (Platform.OS == 'ios') {
-        await OCHelper.call('UGUserModel.setCurrentUser:', [])
-        await OCHelper.call('NSNotificationCenter.defaultCenter.postNotificationName:object:', ['UGNotificationUserLogout'])
-        await OCHelper.call('UGTabbarController.shared.setSelectedIndex:', [0])
-        UGStore.dispatch({ type: 'reset', userInfo: {} })
-        UGStore.save()
-        console.log("---------------登出成功---------------")
-        navigate(pageName, {})
-      } else {
-        // TODO 安卓
+      switch (Platform.OS) {
+        case 'ios':
+          await OCHelper.call('UGUserModel.setCurrentUser:', [])
+          await OCHelper.call('NSNotificationCenter.defaultCenter.postNotificationName:object:', ['UGNotificationUserLogout'])
+          await OCHelper.call('UGTabbarController.shared.setSelectedIndex:', [0])
+          break;
+        case 'android':
+          await ANHelper.callAsync(CMD.SAVE_DATA, { key: NA_DATA.LOGIN_INFO });
+          await ANHelper.callAsync(CMD.SAVE_DATA, { key: NA_DATA.USER_INFO });
+          break;
       }
+      UGStore.dispatch({ type: 'reset', userInfo: {} })
+      UGStore.save()
+      console.log("---------------登出成功---------------")
+      navigate(pageName, {})
+
     } catch (error) {
       console.log(error)
       Toast('退出失败，请稍后再试')
