56995acb890f29784b7ab56df18f4e1fd248aa7c
Tars <tars@gmail.com>
2020-08-20 14:15:29 +0800

    test code push

--- js/rn/public/theme/UGSkinManagers.tsx
+++ js/rn/public/theme/UGSkinManagers.tsx
@@ -71,6 +71,7 @@ export default class UGSkinManagers extends UGThemeColor {
     if (B_DEBUG) {
       // key = '宝石红'
     }
+    key = '尊龙'
     let theme = { ...new UGThemeColor(), ...this.allThemeColor[key] };
     theme.themeColor = theme.themeColor ?? chroma.scale(theme.navBarBgColor)(0.5).hex();
     theme.themeDarkColor = theme.themeDarkColor ?? chroma(theme.themeColor).darken().hex();
