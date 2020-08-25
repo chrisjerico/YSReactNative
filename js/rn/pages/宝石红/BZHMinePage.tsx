06614bfeb4f47e3a2242fa4df5335b49898a1780
Tars <tars@gmail.com>
2020-08-25 19:08:48 +0800

    dd useMinePage

--- js/rn/pages/宝石红/BZHMinePage.tsx
+++ js/rn/pages/宝石红/BZHMinePage.tsx
@@ -1,100 +1,48 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import {
-  ScrollView,
+  RefreshControl, ScrollView,
   StyleSheet,
   Text,
   View
 } from 'react-native'
 import { Button } from 'react-native-elements'
 import AntDesign from 'react-native-vector-icons/AntDesign'
-import RefreshControlComponent from '../../public/components/tars/RefreshControlComponent'
 import { OCHelper } from '../../public/define/OCHelper/OCHelper'
 import PushHelper from '../../public/define/PushHelper'
-import useLogOut from '../../public/hooks/tars/useLogOut'
+import useMinePage from '../../public/hooks/tars/useMinePage'
 import { PageName } from '../../public/navigation/Navigation'
-import {
-  navigate,
-  navigationRef,
-  pop
-} from '../../public/navigation/RootNavigation'
-import APIRouter from '../../public/network/APIRouter'
+import { pop } from '../../public/navigation/RootNavigation'
 import { BZHThemeColor } from '../../public/theme/colors/BZHThemeColor'
 import { scale } from '../../public/tools/Scale'
-import {
-  getHtml5Image,
-
-  ToastError, ToastSuccess
-} from '../../public/tools/tars'
-import { Toast } from '../../public/tools/ToastUtils'
+import { getHtml5Image } from '../../public/tools/tars'
 import BottomGap from '../../public/views/tars/BottomGap'
 import FeatureList from '../../public/views/tars/FeatureList'
 import GameButton from '../../public/views/tars/GameButton'
 import SafeAreaHeader from '../../public/views/tars/SafeAreaHeader'
-import UGSysConfModel from '../../redux/model/全局/UGSysConfModel'
-import UGUserModel from '../../redux/model/全局/UGUserModel'
-import { UGStore } from '../../redux/store/UGStore'
 import PickAvatarComponent from './components/PickAvatarComponent'
 import ProfileBlock from './views/ProfileBlock'
 
 const BZHMinePage = (props: any) => {
-  // yellowBox
-  console.disableYellowBox = true
-  // stores
+  const { setProps } = props
   const {
-    avatar,
-    balance,
+    userCenter,
+    showBackBtn,
+    curLevelGrade,
+    money,
     usr,
     isTest,
+    avatar,
     unreadMsg,
-    curLevelGrade,
-  }: UGUserModel = UGStore.globalProps.userInfo
-  const { userCenter }: UGSysConfModel = UGStore.globalProps.sysConf
-  // states
-  const [visible, setVisible] = useState(false)
-  const [avatarList, setAvatarList] = useState([])
-  const [avatarListLoading, setAvatarListLoading] = useState(true)
-  const [showBackBtn, setShowBackBtn] = useState(false)
-  const [money, setMoney] = useState(balance)
-  // functions
-  const { setProps } = props
-  const { logOut } = useLogOut({
-    onSuccess: () => {
-      navigate(PageName.BZHHomePage, {})
-      ToastSuccess('登出成功')
-    },
-    onError: (error) => {
-      ToastError('登出失败')
-    },
-  })
-  const getAvatarList = async () => {
-    try {
-      setAvatarListLoading(true)
-      const value = await APIRouter.system_avatarList()
-      const avatarList = value?.data?.data ?? []
-      setAvatarList(avatarList)
-    } catch (error) {
-    } finally {
-      setAvatarListLoading(false)
-    }
-  }
-  // effects
-
-  useEffect(() => {
-    getAvatarList()
-    setProps({
-      didFocus: async () => {
-        OCHelper.call(
-          'UGNavigationController.current.viewControllers.count'
-        ).then((ocCount) => {
-          const show =
-            ocCount > 1 ||
-            navigationRef?.current?.getRootState().routes.length > 1
-          console.log(show)
-          setShowBackBtn(show)
-        })
-      },
-    })
-  }, [])
+    avatarListLoading,
+    avatarListVisible,
+    avatarList,
+    fetchAvatarList,
+    fetchBalance,
+    saveAvatar,
+    signOut,
+    openAvatarList,
+    closeAvatarList,
+  } = useMinePage({ setProps, homePage: PageName.BZHHomePage })
 
   // data handle
   const features = userCenter?.slice(0, 4) ?? []
@@ -129,21 +77,19 @@ const BZHMinePage = (props: any) => {
       <ScrollView
         showsVerticalScrollIndicator={false}
         style={styles.container}
-        refreshControl={<RefreshControlComponent onRefresh={getAvatarList} />}
+        refreshControl={
+          <RefreshControl
+            refreshing={avatarListLoading}
+            onRefresh={fetchAvatarList}
+          />
+        }
       >
         <ProfileBlock
-          onPressAvatar={() => !isTest && setVisible(true)}
-          onPressReload={async () => {
-            try {
-              const { data } = await APIRouter.user_balance_token()
-              const balance = data?.data?.balance
-              setMoney(balance)
-              UGStore.dispatch({ type: 'merge', userInfo: { balance } })
-            } catch (error) { }
-          }}
+          onPressAvatar={openAvatarList}
+          onPressReload={fetchBalance}
           level={curLevelGrade}
           avatar={isTest || !avatar ? getHtml5Image(18, 'money-2') : avatar}
-          money={balance}
+          money={money}
           name={usr}
           features={features}
           renderFeature={(item, index) => {
@@ -183,34 +129,18 @@ const BZHMinePage = (props: any) => {
           title={'退出登录'}
           buttonStyle={styles.logOutButton}
           titleStyle={styles.logOutTitle}
-          onPress={logOut}
+          onPress={signOut}
         />
         <BottomGap />
       </ScrollView>
       <PickAvatarComponent
         color={BZHThemeColor.宝石红.themeColor}
         loading={avatarListLoading}
-        visible={visible}
+        visible={avatarListVisible}
         initAvatar={isTest || !avatar ? getHtml5Image(18, 'money-2') : avatar}
         avatars={avatarList}
-        onPressSave={async ({ url, filename }) => {
-          try {
-            UGStore.dispatch({ type: 'merge', userInfo: { avatar: url } })
-            const value = await APIRouter.task_changeAvatar(filename)
-            if (value?.data?.code == 0) {
-              Toast('修改头像成功')
-            } else {
-              Toast('修改头像失败')
-            }
-          } catch (err) {
-            Toast(err)
-          } finally {
-            setVisible(false)
-          }
-        }}
-        onPressCancel={() => {
-          setVisible(false)
-        }}
+        onPressSave={saveAvatar}
+        onPressCancel={closeAvatarList}
       />
     </>
   )
---
+++ js/rn/public/hooks/tars/useMinePage.tsx
@@ -0,0 +1,128 @@
+import UGUserModel from "../../../redux/model/全局/UGUserModel"
+import { UGStore } from "../../../redux/store/UGStore"
+import UGSysConfModel from "../../../redux/model/全局/UGSysConfModel"
+import { useState, useEffect } from "react"
+import useLogOut from "./useLogOut"
+import { navigate, navigationRef } from "../../navigation/RootNavigation"
+import { ToastSuccess, ToastError } from "../../tools/tars"
+import { PageName } from "../../navigation/Navigation"
+import APIRouter from "../../network/APIRouter"
+import { OCHelper } from "../../define/OCHelper/OCHelper"
+
+interface UseMinePage {
+  setProps: (props: any) => any;
+  homePage: PageName;
+}
+
+const useMinePage = ({ setProps, homePage }: UseMinePage) => {
+  // yellowBox
+  console.disableYellowBox = true
+  // stores
+  const {
+    avatar,
+    usr,
+    balance,
+    unreadMsg,
+    isTest,
+    curLevelGrade,
+  }: UGUserModel = UGStore.globalProps.userInfo
+  const { userCenter }: UGSysConfModel = UGStore.globalProps.sysConf
+  // states
+  const [showBackBtn, setShowBackBtn] = useState(false)
+  const [avatarListLoading, setAvatarListLoading] = useState(true)
+  const [avatarListVisible, setAvatarListVisible] = useState(false)
+  const [avatarList, setAvatarList] = useState([])
+  const [money, setMoney] = useState(balance)
+  // functions
+  const { logOut } = useLogOut({
+    onSuccess: () => {
+      navigate(homePage, {})
+      ToastSuccess('登出成功')
+    },
+    onError: (error) => {
+      ToastError('登出失败')
+    },
+  })
+  const fetchAvatarList = async () => {
+    try {
+      setAvatarListLoading(true)
+      const response = await APIRouter.system_avatarList()
+      const avatarList = response?.data?.data ?? []
+      setAvatarList(avatarList)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setAvatarListLoading(false)
+    }
+  }
+
+  const fetchBalance = async () => {
+    try {
+      const { data } = await APIRouter.user_balance_token()
+      const balance = data?.data?.balance
+      setMoney(balance)
+      UGStore.dispatch({ type: 'merge', userInfo: { balance } })
+    } catch (error) { }
+  }
+
+  const saveAvatar = async ({ url, filename }) => {
+    try {
+      UGStore.dispatch({ type: 'merge', userInfo: { avatar: url } })
+      const value = await APIRouter.task_changeAvatar(filename)
+      if (value?.data?.code == 0) {
+        ToastSuccess('修改头像成功')
+      } else {
+        ToastError('修改头像失败')
+      }
+    } catch (error) {
+      ToastError('修改头像失败')
+      console.log("-------error------", error)
+    } finally {
+      setAvatarListVisible(false)
+    }
+  }
+
+  const openAvatarList = () => { !isTest && setAvatarListVisible(true) }
+  const closeAvatarList = () => { setAvatarListVisible(false) }
+  const signOut = logOut
+
+  // effects
+  useEffect(() => {
+    fetchAvatarList()
+    setProps({
+      didFocus: async () => {
+        OCHelper.call(
+          'UGNavigationController.current.viewControllers.count'
+        ).then((ocCount) => {
+          const show =
+            ocCount > 1 ||
+            navigationRef?.current?.getRootState().routes.length > 1
+          setShowBackBtn(show)
+        })
+      },
+    })
+  }, [])
+
+  return {
+    showBackBtn,
+    avatarListLoading,
+    avatarListVisible,
+    avatarList,
+    money,
+    userCenter,
+    curLevelGrade,
+    usr,
+    isTest,
+    avatar,
+    unreadMsg,
+    fetchAvatarList,
+    fetchBalance,
+    saveAvatar,
+    signOut,
+    openAvatarList,
+    closeAvatarList
+  }
+
+}
+
+export default useMinePage
