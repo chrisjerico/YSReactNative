bb185b4e7eb2ab2f687bfc95d10a6e2d2931da4f
Ezer <ugrnezer@gmail.com>
2020-08-31 13:30:55 +0800

    test bug

--- js/rn/public/components/TrendView.tsx
+++ js/rn/public/components/TrendView.tsx
@@ -273,7 +273,7 @@ const TrendView = () => {
                                       stroke="#409fdc" strokeWidth="1"/>
                         })}
                     </Svg>
-                    
+                    <View/>
                 </ScrollView>
             </ScrollView>
             <View style={{flexDirection: "row"}}>
