32f0a16a15fb818f217bc7821506d4000de1aed2
Arc <arcline@gmail.com>
2020-09-22 15:23:07 +0800

    白耀

--- js/rn/pages/白耀/utils/fillArray.ts
+++
@@ -1,9 +0,0 @@
-export const fillArray = (data: any[], num: number) => {
-    if (data) {
-        const count = data.length % num;
-        for (let i = 0; i < count; i++) {
-            data.push({} as any);
-        }
-    }
-    return data
-}
