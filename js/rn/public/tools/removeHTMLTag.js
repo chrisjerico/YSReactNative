04a16a9aa62dc851a534f087a2cd45e515483103
Tars <tars@gmail.com>
2020-10-18 15:51:05 +0800

    rm js andjsmap

--- js/rn/public/tools/removeHTMLTag.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.removeHTMLTag = void 0;
-function removeHTMLTag(data) {
-    var regex = /(<([^>]+)>)/ig;
-    return data.replace(regex, '');
-}
-exports.removeHTMLTag = removeHTMLTag;
