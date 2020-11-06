537d651627fa1137d7bd62c1b88bfd3ef1b86a3b
Arc <arcline@gmail.com>
2020-11-06 21:07:16 +0800

    c126域名

--- js/rn/public/config/DomainUrls.tsx
+++ js/rn/public/config/DomainUrls.tsx
@@ -64,7 +64,7 @@ const DomainUrls: {[x:string]:string} = {
   'c114': 'https://c432019.com',
   'c116': 'https://13532038.com',
   'c120': 'https://asafew435yrtgre.net',
-  'c126': 'https://758cpapp.com',
+  'c126': 'https://jzcp055.com',
   'c126b': 'https://og878.vip',
   'c134': 'https://19972030.com',
   'c137': 'https://7033005.com',
