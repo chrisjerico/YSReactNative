3258a91bb9ec4d8298211b33c35a5632e1a698e6
Tars <tars@gmail.com>
2020-11-17 14:32:35 +0800

    fix JXH default icon

--- js/rn/pages/JXH/config.ts
+++ js/rn/pages/JXH/config.ts
@@ -3,8 +3,8 @@ const { getHtml5Image } = useHtml5Image('http://test10.6yc.com/')
 
 const config = {
   defaultUserCenterLogos: {
-    1: getHtml5Image(18, 'ck'), // 存款
-    2: getHtml5Image(18, 'withdrawlogo'), // 取款
+    1: getIbbImage('gZj6n42/money'), // 存款
+    2: getIbbImage('gZj6n42/money'), // 取款
     3: getHtml5Image(18, 'bank'), // 银行卡管理
     4: getHtml5Image(18, 'syb3-1'), // 利息宝
     5: getHtml5Image(18, 'menu-myreco'), // 推荐收益
@@ -16,12 +16,12 @@ const config = {
     11: getHtml5Image(18, 'task'), // 任务中心
     12: getHtml5Image(18, 'userInf'), // 个人信息
     13: getHtml5Image(18, 'menu-feedback'), // 建议反馈
-    14: getHtml5Image(18, 'menu-service'), // 在线客服
-    15: getHtml5Image(18, 'winApply'), // 活动彩金
-    16: getIbbImage('Twnz2tK/changlong'), // 长龙助手
+    14: getIbbImage('VjcSg8s/service'), // 在线客服
+    15: getIbbImage('gmVTZvM/activity'), // 活动彩金
+    16: getIbbImage('4YGRLbs/long'), // 长龙助手
     17: getHtml5Image(18, 'menu-activity'), // 全民竞猜
-    18: getHtml5Image(18, 'kj_trend'), // 开奖走势
-    19: getIbbImage('pw7LqkR/qq'), // QQ客服
+    18: getIbbImage('dt5ShBG/award'), // 开奖走势
+    19: getIbbImage('NV1DZ3M/qq'), // QQ客服
     20: getIbbImage('qRBq508/kjw'), // 開獎網
   },
 }
