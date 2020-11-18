88b38682f16c966bf4043397aec36b526af634a7
Ezer <ugrnezer@gmail.com>
2020-11-18 18:02:15 +0800

    add animated pickerView

--- js/rn/public/components/TransferView.tsx
+++ js/rn/public/components/TransferView.tsx
@@ -1,16 +1,30 @@
-import React from 'react'
-import { FlatList, SafeAreaView, Text, TouchableWithoutFeedback, View } from 'react-native'
+import React, { useState } from 'react'
+import { FlatList, SafeAreaView, Text, TextInput, TouchableWithoutFeedback, View } from 'react-native'
 import AppDefine from '../define/AppDefine'
 import Icon from 'react-native-vector-icons/AntDesign'
+import Animated, {
+  block,
+  Clock, clockRunning, cond, debug,
+  Easing, Extrapolate, interpolate, set, startClock, stopClock, timing, Value,
+} from 'react-native-reanimated'
 
 export const TransferView = () => {
+  const [money, setMoney] = useState(0)
   const mainColor = 'black'
+  const [openPicker, setOpenPicker] = useState(false)
   return (
     <View>
       <Header mainColor={mainColor} pressRecord={() => {
       }} />
-      <MiddleView mainColor={mainColor} balance={0} />
+      <MiddleView mainColor={mainColor} balance={0} money={money} setMoney={setMoney} openPicker={openPicker} setOpenPicker={setOpenPicker} />
       <AccListView />
+{/*      <View style={{
+        backgroundColor: 'rgba(0,0,0, 0.1)',
+        width: AppDefine.width,
+        height: AppDefine.height,
+        position: 'absolute',
+        flex: 1,
+      }} />*/}
     </View>
   )
 }
@@ -48,54 +62,13 @@ const Header = ({ mainColor, pressRecord }: { mainColor: string, pressRecord: ()
   )
 }
 
-const MiddleView = ({ mainColor, balance }: { mainColor: string, balance: string }) => {
+const MiddleView = ({ mainColor, balance, money, setMoney, openPicker, setOpenPicker }:
+                      { mainColor: string, balance: string, money: number, setMoney: (text: string) => void, setOpenPicker: (status: boolean) => void }) => {
   return (
     <View style={{ marginHorizontal: 12, marginTop: 16 }}>
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Text style={{ fontSize: 16, textAlign: 'center' }}>转出钱包</Text>
-        <View style={{
-          flex: 1,
-          marginLeft: 20,
-          borderBottomWidth: 1,
-          borderBottomColor: '#d9d9d9',
-          alignItems: 'center',
-          flexDirection: 'row',
-        }}>
-          <Text style={{ fontSize: 14, paddingVertical: 10 }}>MG电子</Text>
-          <View style={{ flex: 1 }} />
-          <Icon style={{ alignSelf: 'center' }} size={16} name={'caretdown'} />
-        </View>
-      </View>
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Text style={{ fontSize: 16, textAlign: 'center' }}>转出钱包</Text>
-        <View style={{
-          flex: 1,
-          marginLeft: 20,
-          borderBottomWidth: 1,
-          borderBottomColor: '#d9d9d9',
-          alignItems: 'center',
-          flexDirection: 'row',
-        }}>
-          <Text style={{ fontSize: 14, paddingVertical: 10 }}>MG电子</Text>
-          <View style={{ flex: 1 }} />
-          <Icon style={{ alignSelf: 'center' }} size={16} name={'caretdown'} />
-        </View>
-      </View>
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Text style={{ fontSize: 16, textAlign: 'center' }}>转出钱包</Text>
-        <View style={{
-          flex: 1,
-          marginLeft: 20,
-          borderBottomWidth: 1,
-          borderBottomColor: '#d9d9d9',
-          alignItems: 'center',
-          flexDirection: 'row',
-        }}>
-          <Text style={{ fontSize: 14, paddingVertical: 10 }}>MG电子</Text>
-          <View style={{ flex: 1 }} />
-          <Icon style={{ alignSelf: 'center' }} size={16} name={'caretdown'} />
-        </View>
-      </View>
+      <TransferPicker text={'转出钱包'} zIndex={99} setOpenPicker={setOpenPicker}  />
+      <TransferPicker text={'转入钱包'} zIndex={98} setOpenPicker={setOpenPicker} />
+      <TransferPicker text={'转换金额'} zIndex={97} setOpenPicker={setOpenPicker} />
       <View style={{ paddingTop: 32 }}>
         <TouchableWithoutFeedback onPress={() => {
         }}>
@@ -131,13 +104,96 @@ const MiddleView = ({ mainColor, balance }: { mainColor: string, balance: string
 
 const AccListView = () => {
   return (
-    <FlatList style={{marginHorizontal: 12, marginTop: 12}} data={[1,2,3,4,5,6]} renderItem={() => (
-      <View style={{ flexDirection: 'row', marginHorizontal: 8, borderBottomWidth: 1, borderBottomColor: "#d9d9d9", alignItems: 'center', paddingVertical: 12}}>
+    <FlatList style={{ marginHorizontal: 12, marginTop: 12 }} data={[1, 2, 3, 4, 5, 6]} renderItem={() => (
+      <View style={{
+        flexDirection: 'row',
+        marginHorizontal: 8,
+        borderBottomWidth: 1,
+        borderBottomColor: '#d9d9d9',
+        alignItems: 'center',
+        paddingVertical: 12,
+      }}>
         <Icon style={{ alignSelf: 'center' }} size={24} name={'caretdown'} />
-        <Text style={{fontSize: 14, paddingLeft: 16, flex: 1}}>MG电子</Text>
+        <Text style={{ fontSize: 14, paddingLeft: 16, flex: 1 }}>MG电子</Text>
         <Text>￥*****</Text>
         <Icon size={20} name={'reload1'} color={'black'} style={{ marginLeft: 16 }} />
       </View>
     )} />
   )
+}
+
+const TransferPicker = ({ text, zIndex, setOpenPicker, openPicker }: { text: string, zIndex: number, setOpenPicker: (status: boolean) => void, openPicker: boolean }) => {
+  const [open, setOpen] = useState(false)
+  const [animation, setAnimation] = useState(new Value(0))
+  const toggleRow1Content = () => {
+    if (open) {
+      setAnimation(runTiming(new Clock(), new Value(250), new Value(0)))
+      setOpen(false)
+    } else {
+      setAnimation(runTiming(new Clock(), new Value(0), new Value(250)))
+      setOpen(true)
+    }
+  }
+  return (
+    <View style={{ flexDirection: 'row', alignItems: 'center', zIndex }}>
+      <Text style={{ fontSize: 16, textAlign: 'center' }}>{text}</Text>
+      {open && <View style={{
+        backgroundColor: 'rgba(0,0,0, 0.1)',
+        width: AppDefine.width,
+        height: AppDefine.height,
+        position: 'absolute',
+        flex: 1,
+        zIndex: 98,
+        top: 0-180,
+        left: 0-12
+      }} />}
+      <TouchableWithoutFeedback style={{zIndex: 99}} onPress={() => toggleRow1Content()}>
+        <View style={{
+          flex: 1,
+          marginLeft: 20,
+          height: 38,
+          borderBottomWidth: 1,
+          borderBottomColor: '#d9d9d9',
+          alignItems: 'center',
+          flexDirection: 'row',
+        }}>
+          <View style={{ flex: 1 }} />
+          <Icon style={{ alignSelf: 'center' }} size={16} name={'caretdown'} />
+        </View>
+      </TouchableWithoutFeedback>
+      <Animated.View
+        style={{ backgroundColor: 'red', height: animation, width: '78%', position: 'absolute', top: 38, left: 85, zIndex: 99 }} />
+    </View>
+  )
+}
+
+function runTiming(clock, value, dest) {
+  const state = {
+    finished: new Value(0),
+    position: value,
+    time: new Value(0),
+    frameTime: new Value(0),
+  }
+
+  const config = {
+    duration: 500,
+    toValue: dest,
+    easing: Easing.inOut(Easing.cubic),
+  }
+
+  return block([
+
+    cond(clockRunning(clock), 0, [
+
+      set(state.finished, 0),
+      set(state.time, 0),
+      set(state.position, value),
+      set(state.frameTime, 0),
+      set(config.toValue, dest),
+      startClock(clock),
+    ]),
+    timing(clock, state, config),
+    cond(state.finished, debug('stop clock', stopClock(clock))),
+    state.position,
+  ])
 }
