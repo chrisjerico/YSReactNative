c5547049b5716b16e350c4bf2a8d11ace898c80a
Tars <tars@gmail.com>
2020-11-20 17:31:54 +0800

    fix bug

--- js/rn/public/widget/UGLoadingCP.tsx
+++ js/rn/public/widget/UGLoadingCP.tsx
@@ -1,12 +1,13 @@
 import React, { Component, useState, useEffect } from 'react'
 import LinearGradient from 'react-native-linear-gradient'
 import AppDefine from '../define/AppDefine'
-import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, Platform } from 'react-native'
 import FastImage from 'react-native-fast-image'
 import { Skin1 } from '../theme/UGSkinManagers'
 import { Button, Icon } from 'react-native-elements'
 import Animated, { Easing } from 'react-native-reanimated'
 import { Res } from '../../Res/icon/Resources'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 export enum UGLoadingType {
   Message, // 文本提示
@@ -92,13 +93,12 @@ export const UGLoadingCP1 = (props: UGLoadingProps) => {
   const { type, text, backgroundColor = ['transparent', 'transparent'], reloadClick, setHideLoading, pointerEvents } = props
   const [zIndex, setZIndex] = useState(0)
   const fadeInOpacity = new Animated.Value(0)
-  const hideLoadingCP1 = () => {
+  const hideLoading = () => {
     Animated.timing(fadeInOpacity, { toValue: 0, duration: 250, easing: Easing.linear }).start() // 淡出
     setTimeout(() => {
       lastProps === props && setZIndex(-999)
     }, 250) // 开启点击穿透
     UGLoadingProps.shared = undefined
-    hideLoading()
   }
 
   // 显示新样式（重新初始化）
@@ -110,27 +110,30 @@ export const UGLoadingCP1 = (props: UGLoadingProps) => {
   // 初始化
   useEffect(() => {
     Animated.timing(fadeInOpacity, { toValue: 1, duration: 250, easing: Easing.linear }).start() // 淡入
-    setHideLoading(hideLoadingCP1)
+    setHideLoading(hideLoading)
 
     switch (type) {
       case UGLoadingType.Message:
       case UGLoadingType.Success:
       case UGLoadingType.Error:
         setTimeout(() => {
-          lastProps === props && hideLoadingCP1()
+          lastProps === props && hideLoading()
         }, 3000)
-        break;
       case UGLoadingType.Loading:
         setTimeout(() => {
-          lastProps === props && hideLoadingCP1()
+          lastProps === props && hideLoading()
         }, 20000)
-        break;
+      default:
     }
   }, [lastProps])
 
   return (
     <Animated.View style={[{ position: 'absolute', zIndex: zIndex, width: AppDefine.width, height: AppDefine.height }, { opacity: fadeInOpacity }]} pointerEvents={pointerEvents ? 'none' : 'auto'}>
-      <LinearGradient colors={backgroundColor} start={{ x: 0, y: 1 }} end={{ x: 1, y: 1 }} style={{ flex: 1, justifyContent: 'center' }}>
+      <LinearGradient
+        colors={backgroundColor}
+        start={{ x: 0, y: 1 }}
+        end={{ x: 1, y: 1 }}
+        style={{ flex: 1, justifyContent: 'center', backgroundColor: backgroundColor?.length == 1 ? backgroundColor[0] : undefined }}>
         <View style={{ marginHorizontal: 30, marginTop: -50, backgroundColor: '#eee', alignSelf: 'center', borderRadius: 10, padding: 13 }}>
           {type == UGLoadingType.Loading && <FastImage style={[styles.icon, { width: 50, height: 50 }]} source={Res.加载中} />}
           {type == UGLoadingType.Success && <FastImage style={[styles.icon, { width: 30, height: 30 }]} source={Res.加载成功} />}
@@ -146,7 +149,7 @@ export const UGLoadingCP1 = (props: UGLoadingProps) => {
           )}
           {type == UGLoadingType.Reload && (
             <View style={{ position: 'absolute', alignSelf: 'flex-end', marginTop: -7, marginLeft: 30 }}>
-              <TouchableOpacity style={[{ width: 44, height: 44 }, { overflow: 'hidden' }]} onPress={hideLoadingCP1}>
+              <TouchableOpacity containerStyle={[{ width: 44, height: 44 }, { overflow: 'hidden' }]} onPress={hideLoading}>
                 <Icon size={30} type="antdesign" name="closecircleo" color="black" containerStyle={{ alignSelf: 'flex-end', marginRight: -7, backgroundColor: '#eee', borderRadius: 15 }} />
               </TouchableOpacity>
             </View>
