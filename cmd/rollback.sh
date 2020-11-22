96f75f31a6f41e184c1b1aef2cd103f2f36b40d6
Arc <arcline@gmail.com>
2020-11-22 22:21:37 +0800

    脚本调整

--- cmd/rollback.sh
+++ cmd/rollback.sh
@@ -1,6 +1,24 @@
 cd ..
 
-#渠道: 正式 Production，测试 Staging
+#回滚的渠道: Staging, Arc, Pyro, Smith, Ian, a002, c001 等等
+#回滚所有渠道，输入: all
+
 pub_type=$1
 
-code-push rollback UGBWApp $pub_type
+if [ "$type" == "all" ]; then
+	all_sites=cmd/all_sites.txt
+	string=$(cat ${all_sites})  
+	array=(${string//,/ })  
+	 
+	for var in ${array[@]}
+	do
+	   code-push rollback UGBWApp $var
+	done
+else
+	array=(${pub_type//,/ })
+	 
+	for var in ${array[@]}
+	do
+	   code-push rollback UGBWApp $var
+	done
+fi
