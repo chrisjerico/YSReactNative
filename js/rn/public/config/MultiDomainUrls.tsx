e7ad0138d408c51dcb898a06e152ffc7aa28fb13
ugtmac <syou66699@email.com>
2020-12-05 16:07:44 +0800

    https://center001.fhptbet.net/gd.php?action=view&id=133037 133037】a002】a002  苹果  原生APP   接口域名 轮询c49zq.com c49ln.com c49nx.com c49xg

--- js/rn/public/config/MultiDomainUrls.tsx
+++ js/rn/public/config/MultiDomainUrls.tsx
@@ -120,7 +120,11 @@ const MultiDomainUrls: { [x: string]: Array<string> } = {
     'http://test03.6yc.com',
   ],
   'a002': [
-    'https://5049uuu.com',
+    'https://c49zq.com',
+    'https://c49ln.com',
+    'https://c49nx.com',
+    'https://c49xg.com',
+    'https://5049ttt.com',
   ],
   'c001': [
     'https://c47c47appapp.com',
