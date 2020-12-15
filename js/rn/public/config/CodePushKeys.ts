ddfc1f7233c2e8d30d4b5cafd700a1e421d2b272
fish <gold.fish.a1b1@gmail.com>
2020-12-15 16:50:09 +0800

    新增c259热更新Key；

--- js/rn/public/config/CodePushKeys.ts
+++ js/rn/public/config/CodePushKeys.ts
@@ -60,6 +60,7 @@ const CodePushKeysForIOS = {
   c252: 'spzTpSgi5H7ebn9SV7pZ4XV4oZl54ksvOXqog',
   c254: 'NQoF3CTLMIK3l34dlDCUTeVcmUUG4ksvOXqog',
   c257: 'YmkUvFvTTxCKblY9MxGBASL7XGkc4ksvOXqog',
+  c259:'cP9P5NmuUrCWz7jOyGyEP1S64gFj4ksvOXqog',
   h003b: '6Ezn6jgJhZnOwslpQCeBp2hDFO6b4ksvOXqog',
   l002: '3pcbblg7f9ZygsdecDisvV2LwTRv4ksvOXqog',
 }
