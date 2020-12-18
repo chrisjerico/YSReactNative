1350a2017a0d7c3cdc574d5ed7a33c083a4cdc15
Arc <arclinde@gmail.com>
2020-12-18 12:42:10 +0800

    默认菜单

--- js/rn/Res/icon/Res.tsx
+++ js/rn/Res/icon/Res.tsx
@@ -2,6 +2,11 @@
 // https://appstatic.woyovr.com
 // https://appstatic.guolaow.com
 
+/**
+ * 全局资源统一管理
+ *
+ */
+
 const RES_BASE_URL = 'https://appstatic.guolaow.com/'
 export const ROULETTE_LOGO = RES_BASE_URL + 'assets/c018dzp.gif'    //大转盘
 
