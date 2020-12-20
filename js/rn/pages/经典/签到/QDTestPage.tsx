f4867e5b73f661cc35575d7a5a6676815651ace3
andrew <andrew@gmail.com>
2020-12-20 22:58:01 +0800

    2

--- js/rn/pages/经典/签到/QDTestPage.tsx
+++ js/rn/pages/经典/签到/QDTestPage.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList, StyleSheet,RefreshControl, Image, ImageBackground } from 'react-native';
+import { View, Text, FlatList, StyleSheet, RefreshControl, Image, ImageBackground } from 'react-native';
+import { Button } from 'react-native-elements';
+import { ScrollView } from 'react-native-gesture-handler';
 import AppDefine from '../../../public/define/AppDefine';
 import { useHtml5Image } from '../../../public/tools/tars';
 
@@ -7,45 +9,46 @@ const { getHtml5Image, getHtml5ImagePlatform } = useHtml5Image('http://test10.6y
 
 
 export default class QDTestPage extends Component {
- 
+
     state = {
         list: [],
         refreshing: false
     };
- 
-    componentDidMount(){
+
+    componentDidMount() {
         // 初始化数据
         let list = [];
-        for(var i = 0; i < 7; i++) {
-            list.push({key: 'key'+(i+1)});
+        for (var i = 0; i < 7; i++) {
+            list.push({ key: 'key' + (i + 1) });
         }
-        this.setState({list: list});
+        this.setState({ list: list });
     }
- 
+
     // 渲染列表项
-    _renderItem = ({index, item}) => {
+    _renderItem = ({ index, item }) => {
         console.log(index);
         console.log('img = ', getHtml5ImagePlatform(undefined, 'static/vueTemplate/vue/images/my/userInfo/signed'));
+
         return (
             <View key={item.key} style={styles.itemViewStyle}>
                 <Text style={[styles.itemTextStyle, styles.itemTextSizeStyle]}>{item.key}</Text>
                 <Text style={[styles.itemTextStyle, styles.itemTextSizeStyle]}>{item.key}</Text>
-                <ImageBackground style={styles.itemImageStyle } source={{uri: 'https://appstatic.woyovr.com/assets/sigined.png'}}>
-                     <Text style={[styles.itemImageTextStyle, styles.itemTextSizeStyle]}>{item.key}</Text>
-                     <Image style={[styles.itemImageImageStyle ]} source={{uri: 'https://appstatic.woyovr.com//web/static/vueTemplate/vue/images/my/userInfo/sign/gold.png'}}/>
-                     <ImageBackground style={styles.itemImageImage2Style } source={{uri: 'https://appstatic.woyovr.com/assets/signIn_grey.png'}}>
-                     <Text style={[styles.itemImageImageTextStyle, styles.itemTextSizeStyle]}>{item.key}</Text>
-                     </ImageBackground>
+                <ImageBackground style={[styles.itemImageStyle, { borderRadius: 5, overflow: 'hidden' }]} source={{ uri: 'https://appstatic.guolaow.com/web/static/vueTemplate/vue/images/my/userInfo/sign/signed.png' }}>
+                    <Text style={[styles.itemImageTextStyle, styles.itemTextSizeStyle]}>{item.key}</Text>
+                    <Image style={[styles.itemImageImageStyle]} source={{ uri: 'https://appstatic.guolaow.com/web/static/vueTemplate/vue/images/my/userInfo/sign/gold.png' }} />
+                    <ImageBackground style={[styles.itemImageImage2Style,]} source={{ uri: 'https://appstatic.guolaow.com/assets/signIn_blue.png' }}>
+                        <Text style={[styles.itemImageImageTextStyle, styles.itemImageImageTextSizeStyle]}>{item.key}</Text>
+                    </ImageBackground>
                 </ImageBackground>
             </View>
         );
     }
- 
+
     // 分割线
     _renderSeparator = () => {
         return (
             class Separator extends Component {
-                render(){
+                render() {
                     return (
                         <View style={styles.separatorStyle} />
                     );
@@ -53,84 +56,122 @@ export default class QDTestPage extends Component {
             }
         );
     }
- 
+
     _renderListEmptyComp = () => {
         return (
             <View>
-                 <Text>没有数据时显示本段文字</Text>
+                <Text>没有数据时显示本段文字</Text>
             </View>
         );
     }
- 
+
     // 底部加载
     _onEndReached = () => {
-        this.setState({refreshing: true});
+        this.setState({ refreshing: true });
         // 关于更新state里数组的两种方式
         //setState({ 'arrary': [...this.state.array, newItem]}).
         //setState({ 'array' : [...this.state.array].concat(newList|newItem)}).
         let newList = [];
-        for(var i = 0; i < 3; i++) {
-            newList.push({key: '(new)key'+ Math.floor(Math.random() * 10000)});
+        for (var i = 0; i < 3; i++) {
+            newList.push({ key: '(new)key' + Math.floor(Math.random() * 10000) });
         }
- 
-        setTimeout(()=>{
-            this.setState({list: [...this.state.list].concat(newList), refreshing: false});
-        },2000);
+
+        setTimeout(() => {
+            this.setState({ list: [...this.state.list].concat(newList), refreshing: false });
+        }, 2000);
     }
- 
+
     // 顶部加载
     _onRefresh = () => {
-        this.setState({refreshing: true});
-        setTimeout(()=>{
-            this.setState({refreshing: false});
+        this.setState({ refreshing: true });
+        setTimeout(() => {
+            this.setState({ refreshing: false });
             // this.myFlatList.scrollToEnd(); // 滚动到底部
             // this.myFlatList.scrollToIndex({animated: true, index:10}); // 将位于索引值为index的元素滚动到可视区域首行位置
             // this.myFlatList.flashScrollIndicators(); // 短暂地显示滚动指示器
-        },2000);    
+        }, 2000);
     }
- 
+
     render() {
         console.log(this.state.list);
         return (
-            <View style={{flex:1}}>
+            <View style={{ flex: 1 }}>
                 <View style={styles.headerViewStyle}>
                     <Text style={styles.headerTextStyle}>我的APP</Text>
                 </View>
-                <FlatList
-                    style={styles.scrollViewStyle}
-                    ref={(view) => { this.myFlatList = view; }}
-                    data={this.state.list} // 数据源
-                    renderItem={this._renderItem} // 从数据源中挨个取出数据并渲染到列表中
-                    showsVerticalScrollIndicator={false} // 当此属性为true的时候，显示一个垂直方向的滚动条，默认为: true
-                    showsHorizontalScrollIndicator={false} // 当此属性为true的时候，显示一个水平方向的滚动条，默认为: true
-                    // ItemSeparatorComponent = {this._renderSeparator()} // 行与行之间的分隔线组件。不会出现在第一行之前和最后一行之后
-                    ListEmptyComponent = {this._renderListEmptyComp()} // 列表为空时渲染该组件。可以是 React Component, 也可以是一个 render 函数，或者渲染好的 element
-                    // onEndReachedThreshold={0.01} // 决定当距离内容最底部还有多远时触发onEndReached回调，范围0~1，如0.01表示触底时触发
-                    // onEndReached={this._onEndReached} // 在列表底部往下滑时触发该函数。表示当列表被滚动到距离内容最底部不足onEndReachedThreshold的距离时调用
-                    numColumns = {4}
-                    // refreshControl={
-                    //     <RefreshControl
-                    //         // refreshing={this.state.refreshing} // 在等待加载新数据时将此属性设为 true，列表就会显示出一个正在加载的符号
-                    //         // onRefresh={this._onRefresh.bind(this)} // 在列表顶部往下滑时触发该函数。如果设置了此选项，则会在列表头部添加一个标准的RefreshControl控件，以便实现“下拉刷新”的功能
-                    //         // tintColor="#ffffff" // 指定刷新指示器的背景色(iOS)
-                    //         // title="加载中..." // 指定刷新指示器下显示的文字(iOS)
-                    //         // titleColor="#000000" // 指定刷新指示器下显示的文字的颜色(iOS)
-                    //         // colors={['#ff0000', '#00ff00', '#0000ff']} // 刷新指示器在刷新期间的过渡颜色(Android)
-                    //         // progressBackgroundColor="#ffffff" // 指定刷新指示器的背景色(Android)
-                    //     />
-                    // }
-                />
+
+                <ScrollView onScroll={() => { }}>
+
+                    <View style={[{ height: 200, backgroundColor: '#0000FF' }]}>
+                        <View style={{ flexDirection: 'row', marginTop: 5, justifyContent: 'center' }}>
+                            <Text style={[{ fontSize: 41, color: '#FBF2D5' }]}>{'签到领积分'}</Text>
+                            <View style={[{  marginTop: 16, backgroundColor:'#FFAA2F',borderRadius: 5,}]}>
+                                <Text style={[{ fontSize: 13, color: 'white',marginHorizontal:10 , marginVertical:5, }]}>{'用 积 分 兑 换 现 金'}</Text>
+                            </View>
+
+                        </View>
+                    </View>
+
+
+
+                    <View style={[{
+                        borderRadius: 5, overflow: 'hidden', borderColor: '#EDFAFE', height: 400, borderWidth: 4, marginLeft: 5,
+                        marginRight: 5,
+                    }]}>
+                        <FlatList
+                            style={[styles.scrollViewStyle]}
+                            ref={(view) => { this.myFlatList = view; }}
+                            data={this.state.list} // 数据源
+                            renderItem={this._renderItem} // 从数据源中挨个取出数据并渲染到列表中
+                            showsVerticalScrollIndicator={false} // 当此属性为true的时候，显示一个垂直方向的滚动条，默认为: true
+                            scrollEnabled={false}
+                            showsHorizontalScrollIndicator={false} // 当此属性为true的时候，显示一个水平方向的滚动条，默认为: true
+                            // ItemSeparatorComponent = {this._renderSeparator()} // 行与行之间的分隔线组件。不会出现在第一行之前和最后一行之后
+                            ListEmptyComponent={this._renderListEmptyComp()} // 列表为空时渲染该组件。可以是 React Component, 也可以是一个 render 函数，或者渲染好的 element
+                            // onEndReachedThreshold={0.01} // 决定当距离内容最底部还有多远时触发onEndReached回调，范围0~1，如0.01表示触底时触发
+                            // onEndReached={this._onEndReached} // 在列表底部往下滑时触发该函数。表示当列表被滚动到距离内容最底部不足onEndReachedThreshold的距离时调用
+                            numColumns={4}
+                        // refreshControl={
+                        //     <RefreshControl
+                        //         // refreshing={this.state.refreshing} // 在等待加载新数据时将此属性设为 true，列表就会显示出一个正在加载的符号
+                        //         // onRefresh={this._onRefresh.bind(this)} // 在列表顶部往下滑时触发该函数。如果设置了此选项，则会在列表头部添加一个标准的RefreshControl控件，以便实现“下拉刷新”的功能
+                        //         // tintColor="#ffffff" // 指定刷新指示器的背景色(iOS)
+                        //         // title="加载中..." // 指定刷新指示器下显示的文字(iOS)
+                        //         // titleColor="#000000" // 指定刷新指示器下显示的文字的颜色(iOS)
+                        //         // colors={['#ff0000', '#00ff00', '#0000ff']} // 刷新指示器在刷新期间的过渡颜色(Android)
+                        //         // progressBackgroundColor="#ffffff" // 指定刷新指示器的背景色(Android)
+                        //     />
+                        // }
+                        />
+
+                    </View>
+
+                    <View style={{ alignItems: 'center', marginTop: -20, justifyContent: 'center' }}>
+                        <Button title={'马上签到'} containerStyle={{ width: 140, height: 40, backgroundColor: '#0000FF', borderRadius: 25, overflow: 'hidden' }} titleStyle={{ color: 'white', fontSize: 22 }}
+                            onPress={() => {
+                                console.log('点击了')
+                            }} />
+                    </View>
+
+
+                    <View style={[{ height: 500, backgroundColor: '#0000FF' }]}>
+
+                    </View>
+
+
+                </ScrollView>
+
             </View>
         );
     }
 }
- 
+
 const styles = StyleSheet.create({
     scrollViewStyle: {
-        flex: 1, 
-        marginLeft:10, 
-        marginRight: 10, 
-        marginBottom: 10
+        flex: 1,
+        marginLeft: 10,
+        marginRight: 10,
+        // backgroundColor: '#7B68EE',
     },
     headerViewStyle: {
         height: 50,
@@ -144,9 +185,9 @@ const styles = StyleSheet.create({
     },
     itemViewStyle: {
         height: 185,
-        width: (AppDefine.width - 50)/4,
-        borderWidth: 1,
-        borderRadius: 10,
+        width: (AppDefine.width - 50 - 20) / 4,
+        // borderWidth: 1,
+        // borderRadius: 10,
         marginTop: 5,
         marginLeft: 4,
         marginRight: 4,
@@ -158,7 +199,7 @@ const styles = StyleSheet.create({
         fontSize: 13
     },
     itemTextSizeStyle: {
-        marginTop: 5,
+        marginTop: 8,
         alignItems: 'center'//控制子元素水平居中
     },
     itemImageStyle: {
@@ -166,11 +207,13 @@ const styles = StyleSheet.create({
         width: 80,
         marginTop: 5,
         resizeMode: "stretch",
-        alignItems: 'center' 
+        alignItems: 'center',
+        // borderWidth: 1,
+        // borderRadius: 10,
     },
     itemImageTextStyle: {
-        color: 'black',
-        fontSize: 18
+        color: 'white',
+        fontSize: 15
     },
     itemImageImageStyle: {
         height: 27,
@@ -183,11 +226,15 @@ const styles = StyleSheet.create({
         width: 60,
         marginTop: 10,
         resizeMode: "stretch",
-        alignItems: 'center' 
+        alignItems: 'center'
     },
     itemImageImageTextStyle: {
-        color: 'black',
+        color: 'white',
         fontSize: 13
+    },
+    itemImageImageTextSizeStyle: {
+        alignItems: 'center',//控制子元素水平居中
+        justifyContent: 'center',//控制子元素垂直居中
     },
     separatorStyle: {
         borderColor: '#A4A4A4',
@@ -195,4 +242,4 @@ const styles = StyleSheet.create({
         marginTop: 5
     }
 });
- 
+
