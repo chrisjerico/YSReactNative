4a89981be9f550c9f50f7f36ea739fe236cfb973
Arc <arclinde@gmail.com>
2020-12-23 14:48:03 +0800

    开奖结果

--- js/rn/pages/hall/new/GameHallPage.tsx
+++ js/rn/pages/hall/new/GameHallPage.tsx
@@ -153,7 +153,7 @@ const GameHallPage = ({ navigation, setProps }) => {
           color={'white'}/>
   </TouchableWithoutFeedback>
 
-  const menuStr = [['即时注单', userInfo?.unsettleAmount], ['今日已结'], ['开奖结果'], ['提现']]
+  const menuStr = [['即时注单', userInfo?.unsettleAmount], ['今日已结'], ['开奖记录'], ['提现']]
   /**
    * 绘制菜单
    * @param index
@@ -168,7 +168,7 @@ const GameHallPage = ({ navigation, setProps }) => {
         PushHelper.pushUserCenterType(UGUserCenterType.彩票注单记录)
         break
       case 2:
-        PushHelper.pushUserCenterType(UGUserCenterType.彩票注单记录)
+        PushHelper.pushUserCenterType(UGUserCenterType.开奖结果)
         break
       case 3:
         PushHelper.pushUserCenterType(UGUserCenterType.取款)
