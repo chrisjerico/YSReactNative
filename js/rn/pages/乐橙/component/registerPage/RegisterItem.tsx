5a8e6c60b4c69a2b75dc77ea48a88f8316c84b35
Ezer <ugrnezer@gmail.com>
2020-12-26 15:14:03 +0800

    fix LC bug

--- js/rn/pages/乐橙/component/registerPage/RegisterItem.tsx
+++ js/rn/pages/乐橙/component/registerPage/RegisterItem.tsx
@@ -1,11 +1,61 @@
-import { TextInput, View } from 'react-native'
+import { Alert, Image, StyleSheet, Text, TextInput, TouchableWithoutFeedback, View } from 'react-native'
 import * as React from 'react'
 import { Icon } from 'react-native-elements'
 import { scale } from '../../../../public/tools/Scale'
 import Button from '../../../../public/views/tars/Button'
+import FastImage from 'react-native-fast-image'
+import { useEffect, useState } from 'react'
+import { hideLoading, showLoading } from '../../../../public/widget/UGLoadingCP'
+import APIRouter from '../../../../public/network/APIRouter'
+import { ToastError, ToastSuccess } from '../../../../public/tools/tars'
 
-export const RegisterItem = ({ config, placeHolder, iconName, iconType = 'font-awesome', onChangeText, onPressSms }:
-                               { config?: any, placeHolder: string, iconName: string, iconType?: string, onChangeText: (text) => void, onPressSms?: () => void }) => {
+export const RegisterItem = ({ config, placeHolder, iconName, iconType = 'font-awesome', onChangeText, phoneNumber }:
+                               { config?: any, placeHolder: string, iconName: string, iconType?: string, onChangeText: (text) => void, phoneNumber?: string }) => {
+  const [disableSms, setDisableSms] = useState(false)
+  const [sec, setSec] = useState(60)
+  let interval
+
+  useEffect(() => {
+    let counter = 60
+    interval = disableSms && setInterval(() => {
+      if (counter == 0) {
+        setDisableSms(false)
+        clearInterval(interval)
+        setSec(60)
+      } else {
+        counter = counter - 1
+        setSec(counter)
+      }
+    }, 1000)
+
+    return clearInterval(interval)
+
+  }, [disableSms])
+
+  const fetchSms = async (phoneNumber) => {
+    if (phoneNumber) {
+      try {
+        showLoading()
+        setDisableSms(true)
+        const { data } = await APIRouter.secure_smsCaptcha(phoneNumber)
+        const { code, msg } = data ?? {}
+
+        hideLoading()
+        if (code != 0) {
+          throw { message: msg }
+        } else {
+          ToastSuccess(msg)
+        }
+      } catch (error) {
+        hideLoading()
+        ToastError(error?.message)
+        setDisableSms(false)
+      }
+    } else {
+      Alert.alert('请填写手机号')
+    }
+  }
+
   return (
     <>
       {config === false || config == 0 || config == '0' ?
@@ -21,18 +71,33 @@ export const RegisterItem = ({ config, placeHolder, iconName, iconType = 'font-a
           <Icon type={iconType} size={25} color={'gold'} name={iconName} />
           <TextInput onChangeText={(text) => onChangeText(text)} placeholder={placeHolder}
                      style={{ flex: 1, marginLeft: 12 }} />
-          {placeHolder == '请输入手机短信验证码' && <Button
-            title={'获取验证码'}
-            onPress={onPressSms}
-            titleStyle={{ fontSize: scale(20) }}
-            containerStyle={{
+          {placeHolder == '请输入手机短信验证码' &&
+          <TouchableWithoutFeedback onPress={() => disableSms ? null : fetchSms(phoneNumber)}>
+            <View style={disableSms ? [styles.disabledContainer] : [styles.container, {
               aspectRatio: 4,
               width: scale(150),
               backgroundColor: '#F1E1FF',
               borderRadius: scale(5),
-            }}
-          />}
+            }]}>
+              <Text style={[styles.title, { fontSize: scale(20) }]} numberOfLines={1}>
+                {disableSms ? '重新获取' + sec : '获取验证码'}
+              </Text>
+            </View>
+          </TouchableWithoutFeedback>}
         </View>
       }
     </>)
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {},
+  disabledContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#c6c6c6',
+  },
+})
