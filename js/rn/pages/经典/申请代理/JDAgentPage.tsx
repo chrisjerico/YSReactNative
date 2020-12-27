492a158715d7111d4f356c57a9f1e3244cdca5d6
andrew <andrew@gmail.com>
2020-12-27 16:01:44 +0800

    1

--- js/rn/pages/经典/申请代理/JDAgentPage.tsx
+++ js/rn/pages/经典/申请代理/JDAgentPage.tsx
@@ -11,38 +11,48 @@ import PushHelper from '../../../public/define/PushHelper';
 import { UGUserCenterType } from '../../../redux/model/全局/UGSysConfModel';
 import { push } from '../../../public/navigation/RootNavigation';
 import { PageName } from '../../../public/navigation/Navigation';
+import { UGAgentApplyInfo } from "../../../redux/model/全局/UGSysConfModel";
+import { api } from '../../../public/network/NetworkRequest1/NetworkRequest1';
 
 interface JDAgentPage {
   reviewStatus?: number //0,1 未提交、审核中(输入) 3 审核拒绝 （显示）
 }
-const JDAgentPage =({ setProps })=>{
+const JDAgentPage =({ })=>{
 
   const [reviewStatus, setReviewStatus] = useState<number>(0)//控制View 隐藏显示
+  const [agentApplyInfo, setAgentApplyInfo] = useState<UGAgentApplyInfo>()//签到数据（全部）
 
+   //代理申请信息
+   function teamAgentApplyInfo() {
+    api.team.agentApplyInfo().setCompletionBlock(({ data, msg }) => {
+         setAgentApplyInfo(data)
+         console.log('代理申请信息数据：=', data);
+        setReviewStatus(data.reviewStatus)
+
+    }, (err) => {
+        console.log('err = ', err);
+        // Toast(err.message)
+
+    });
+}
   useEffect(() => {
-
+    teamAgentApplyInfo()
     setProps({
       navbarOpstions: { hidden: false, title: '申请代理' },
-      didFocus: (params) => {
-        let dic = params;
-        // console.log("输出最初的字典元素: "); 
-        for (var key in dic) {
-          // console.log("key: " + key + " ,value: " + dic[key]);
-          if (key == 'feedType') {
-            setReviewStatus(dic[key])
-          }
-
-        }
+      didFocus: () => {
+        AppDefine.checkHeaderShowBackButton((show) => {
+          setProps({ navbarOpstions: { back: show } });
+        })
       }
     })
 
   }, [])
 
   return(
-    <View>
-      //输入界面
-      {reviewStatus == 0 || reviewStatus == 1 && <View style={{height: 200,backgroundColor:'red'}}></View>}
-      //显示界面
+    <View style={{}}>
+      {/* 输入界面 */}
+     {reviewStatus == 0 || reviewStatus == 1 && <View style={{height: 200,backgroundColor:'red'}}></View>}
+      {/* 展示界面 */}
       {reviewStatus == 3 && <View style={{height: 200,backgroundColor:'yellow'}}></View>}
     </View>
 
