baa6612f432424247447ac2cff2b3963010a537c
andrew <andrew@gmail.com>
2021-01-05 21:25:05 +0800

    1

--- js/rn/pages/经典/推荐收益/JDPromotionInfoCP.tsx
+++ js/rn/pages/经典/推荐收益/JDPromotionInfoCP.tsx
@@ -52,9 +52,9 @@ const JDPromotionInfoCP = ({ route, setProps }: UGBasePageProps) => {
     if ('c186' == AppDefine.siteId) {
       returnStr = '方案一：佣金比例图如上，有效投注达到100万以上，将可赚取0.1%的佣金【100万X0.001=1000】1000元佣金!有效投注越高，佣金就越高，亏损分红达到1万以上，另可再次得到1%佣金，【10000X0.1=1000】1000元亏损分红！'
     } else {
-      let proportion: number = parseFloat(inviteInfoModel.fandian)
+      let proportion: number = parseFloat(inviteInfoModel?.fandian)
       let jg: number = proportion * 1000 / 100;
-      returnStr = '您推荐的会员在下注结算后，佣金会自动按照比例加到您的资金账户上。例如：您所推荐的会员下注1000元，您的收益=1000元*(一级下线比例比如：' + inviteInfoModel.fandian + '%)=' + jg.toFixed(2) + '元。'
+      returnStr = '您推荐的会员在下注结算后，佣金会自动按照比例加到您的资金账户上。例如：您所推荐的会员下注1000元，您的收益=1000元*(一级下线比例比如：' + inviteInfoModel?.fandian + '%)=' + jg.toFixed(2) + '元。'
     }
     return returnStr;
   }
@@ -96,7 +96,7 @@ const JDPromotionInfoCP = ({ route, setProps }: UGBasePageProps) => {
       returnNumber = AppDefine.width * 15.88 / 24.34;
     }
     else {
-      returnNumber =  256;
+      returnNumber = AppDefine.width * 740 / 994;
     }
     console.log('returnNumber = ',returnNumber);
     
@@ -117,21 +117,21 @@ const JDPromotionInfoCP = ({ route, setProps }: UGBasePageProps) => {
     <ScrollView style={{ backgroundColor: Skin1.textColor4 }}>
       {/* 输入界面 */}
       {myreco_img == '0' && <Image style={[{ height: headImgHeight(), width: '100%', }]} source={{ uri: headImg() }} />}
-      <JDPromotionInfoText1CP title={'我的用户名:'} content={inviteInfoModel.username} />
-      <JDPromotionInfoText1CP title={'我的推荐ID:'} content={inviteInfoModel.rid} />
+      <JDPromotionInfoText1CP title={'我的用户名:'} content={inviteInfoModel?.username} />
+      <JDPromotionInfoText1CP title={'我的推荐ID:'} content={inviteInfoModel?.rid} />
       <JDPromotionInfoText2CP content={'如果想赚取丰厚的推荐佣金，请复制推荐链接发给您的好友注册。（推荐网址请用浏览器打开）'} />
       <JDPromotionInfoCopyCP title={'首页推荐地址'}
-        content={inviteInfoModel.link_i}
+        content={inviteInfoModel?.link_i}
         imgUrl={'https://appstatic.guolaow.com/web/images/zxkf.png'}
       />
       <JDPromotionInfoText2CP content={'如果想赚取丰厚的推荐佣金，请复制推荐链接发给您的好友注册。（推荐网址请用浏览器打开）'} />
       <JDPromotionInfoCopyCP title={'注册推荐地址'}
-        content={inviteInfoModel.link_i}
+        content={inviteInfoModel?.link_i}
         imgUrl={'https://appstatic.guolaow.com/web/images/zxkf.png'}
       />
-      <JDPromotionInfoText1CP title={'本月推荐收益:'} content={inviteInfoModel.month_earn} />
-      <JDPromotionInfoText1CP title={'本月推荐会员:'} content={inviteInfoModel.total_member} />
-      <JDPromotionInfoText1CP title={'推荐会员总计:'} content={inviteInfoModel.month_member} />
+      <JDPromotionInfoText1CP title={'本月推荐收益:'} content={inviteInfoModel?.month_earn} />
+      <JDPromotionInfoText1CP title={'本月推荐会员:'} content={inviteInfoModel?.total_member} />
+      <JDPromotionInfoText1CP title={'推荐会员总计:'} content={inviteInfoModel?.month_member} />
       <JDPromotionInfoText2CP content={yjstring()} />
       {<View style={{ backgroundColor: Skin1.textColor4, height: 100 }} />}
     </ScrollView>
