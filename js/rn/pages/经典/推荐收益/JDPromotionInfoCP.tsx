5f2a18b9a002ee6e864d3216dadc7b3acec3438b
andrew <andrew@gmail.com>
2021-01-05 19:06:56 +0800

    加滚动

--- js/rn/pages/经典/推荐收益/JDPromotionInfoCP.tsx
+++ js/rn/pages/经典/推荐收益/JDPromotionInfoCP.tsx
@@ -1,7 +1,7 @@
 
 
 import React, { useEffect, useState } from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { ScrollView, Text, View,Image } from 'react-native';
 import { Button } from 'react-native-elements';
 import { TextInput } from 'react-native-gesture-handler';
 import AppDefine from '../../../public/define/AppDefine';
@@ -27,6 +27,7 @@ const JDPromotionInfoCP = ({ route, setProps }: UGBasePageProps) => {
     setProps({
       navbarOpstions: { hidden: false, title: '推荐信息', back: true },
       didFocus: () => {
+
       }
     })
 
@@ -35,6 +36,7 @@ const JDPromotionInfoCP = ({ route, setProps }: UGBasePageProps) => {
   return (
     <ScrollView style={{ backgroundColor: Skin1.textColor4 }}>
       {/* 输入界面 */}
+      <Image style={[{ height: 200, width: 200, }]} source={{ uri: ''}} />
 
       <JDPromotionInfoText1CP title={'本月推荐会员:'} content={'一级下线:0，二级下线:0，三级下线:0'} />
       <JDPromotionInfoText2CP content={'您推荐的会员在下注结算后，佣金会自动按照比例加到您的资金账户上。例如：您所推荐的会员下注1000元，您的收益=1000元*(一级下线比例比如：0.15%）=1.5元。'} />
