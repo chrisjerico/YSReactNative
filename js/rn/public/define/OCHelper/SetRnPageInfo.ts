d55cd2371e3d1771b964e2b6f011b0def2d46187
andrew <andrew@gmail.com>
2021-01-10 22:58:16 +0800

    做长龙助手

--- js/rn/public/define/OCHelper/SetRnPageInfo.ts
+++ js/rn/public/define/OCHelper/SetRnPageInfo.ts
@@ -28,7 +28,7 @@ export async function setRnPageInfo(force = false) {
     devConfig?.skinKey && (skitType = devConfig?.skinKey) // 測試開發
     pages.push({
       vcName: 'UGPromotionIncomeController',
-      rnName: PageName.JDPromotionIncomePage,
+      rnName: PageName.JDLotteryAssistantPage,
       fd_prefersNavigationBarHidden: true,
       允许游客访问: true,
       允许未登录访问: true,
