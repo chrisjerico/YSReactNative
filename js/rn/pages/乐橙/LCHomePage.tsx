899e4050567c4ad1a7b27ba7c08fd27ff053c1fd
Ezer <ugrnezer@gmail.com>
2021-01-13 17:33:51 +0800

    fix LC bug

--- js/rn/pages/乐橙/LCHomePage.tsx
+++ js/rn/pages/乐橙/LCHomePage.tsx
@@ -1,12 +1,8 @@
 import { useDimensions } from '@react-native-community/hooks'
 import * as React from 'react'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import {
-  Alert,
   Dimensions,
-  Image,
-  ImageBackground,
-  Platform,
   RefreshControl,
   SafeAreaView,
   ScrollView,
@@ -17,25 +13,19 @@ import {
 import Icon from 'react-native-vector-icons/FontAwesome'
 import { HomeHeaderButtonBar } from './component/homePage/HomeHeaderButtonBar'
 import { HomeTabView } from './component/homePage/homeTabView/HomeTabView'
-import { navigate, push } from '../../public/navigation/RootNavigation'
+import { push } from '../../public/navigation/RootNavigation'
 import { PageName } from '../../public/navigation/Navigation'
-import { OCHelper } from '../../public/define/OCHelper/OCHelper'
-import APIRouter from '../../public/network/APIRouter'
 import FastImage from 'react-native-fast-image'
 import { MarqueeHorizontal } from 'react-native-marquee-ab'
 import PromotionsBlock from '../../public/components/PromotionsBlock'
-import RedBagItem from '../../public/components/RedBagItem'
 import AppDefine from '../../public/define/AppDefine'
-import { NSValue } from '../../public/define/OCHelper/OCBridge/OCCall'
 import PushHelper from '../../public/define/PushHelper'
 import useHomePage from '../../public/hooks/tars/useHomePage'
 import { httpClient } from '../../public/network/httpClient'
 import { List } from '../../public/network/Model/BannerModel'
-import { TurntableListModel } from '../../public/network/Model/TurntableListModel'
 import { scale } from '../../public/tools/Scale'
 import GameButton from '../../public/views/tars/GameButton'
 import RankListCP from '../../public/widget/RankList'
-import { UGStore } from '../../redux/store/UGStore'
 import MarqueePopupView from '../common/MarqueePopupView'
 import NavBlock from './component/homePage/NavBlock'
 import Carousel from 'react-native-banner-carousel'
@@ -51,18 +41,9 @@ const LCHomePage = ({ setProps }) => {
   const { refresh, info } = useHomePage({})
   const { loading, userInfo, sysInfo, homeInfo, refreshing, redBagLogo, isTest } = info
   const { showCoupon } = sysInfo
-  const { homeGames, navs, rankLists, banners, onlineNum, redBag, notices, midBanners, goldenEggs, scratchs, roulette, bannersInterval, floatAds } = homeInfo
-  const { rankingListSwitch, webName, midBannerTimer } = sysInfo
+  const { homeGames, navs, rankLists, banners, onlineNum, redBag, notices, midBanners, goldenEggs, scratchs, roulette, bannersInterval, floatAds, announcements } = homeInfo
+  const { rankingListSwitch, midBannerTimer } = sysInfo
   const { uid } = userInfo
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setProps()
-    }, 3000)
-    return (() => {
-      clearInterval(timer)
-    })
-  }, [])
 
   useEffect(() => {
     if (notices && notices.length > 0 && noticeFormat.length == 0) {
@@ -81,12 +62,19 @@ const LCHomePage = ({ setProps }) => {
     setProps()
   }, [userInfo.uid])
 
+  const onRefresh = useCallback(async () => {
+    try {
+      await refresh()
+      PushHelper.pushAnnouncement(announcements)
+    } catch (error) {}
+  }, [announcements])
+
   return (
     <View style={{ flex: 1, backgroundColor: '#f3f3f3' }}>
       <HomeHeaderButtonBar info={info} />
       <ScrollView showsVerticalScrollIndicator={false}
-                  refreshControl={<RefreshControl style={{ backgroundColor: '#ffffff' }} refreshing={loading}
-                                                  onRefresh={refresh} />}
+                  refreshControl={<RefreshControl style={{ backgroundColor: '#ffffff' }} refreshing={refreshing}
+                                                  onRefresh={onRefresh} />}
       >
         <Banner onlineNum={onlineNum} bannerData={banners} interval={bannersInterval} />
         <View style={{
@@ -195,16 +183,16 @@ const LCHomePage = ({ setProps }) => {
             <RankListCP titleVisible={false} timing={10000} backgroundColor={'white'} textColor={'black'}
                         width={Dimensions.get('screen').width - 24} ranks={rankLists} />
           </SafeAreaView> : <></>}
-        <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 10 }}>
-          <Text onPress={() => {
-            PushHelper.openWebView(httpClient.defaults.baseURL + '/index2.php')
-          }} style={{ color: 'black', textAlign: 'center', marginRight: 20, marginBottom: 5 }}>💻 电 脑 版</Text>
-          <Text style={{ color: 'black', textAlign: 'center' }} onPress={() => {
-            push(PageName.PromotionListPage)
-          }}>🎁优惠活动</Text>
-        </View>
-        <Text style={{ color: 'black', textAlign: 'center' }}>COPYRIGHT © {webName} RESERVED</Text>
-        <Text style={{ color: '#000000', textAlign: 'center' }}>{'VERSION : 13'}</Text>
+        {/*<View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 10 }}>*/}
+        {/*  <Text onPress={() => {*/}
+        {/*    PushHelper.openWebView(httpClient.defaults.baseURL + '/index2.php')*/}
+        {/*  }} style={{ color: 'black', textAlign: 'center', marginRight: 20, marginBottom: 5 }}>💻 电 脑 版</Text>*/}
+        {/*  <Text style={{ color: 'black', textAlign: 'center' }} onPress={() => {*/}
+        {/*    push(PageName.PromotionListPage)*/}
+        {/*  }}>🎁优惠活动</Text>*/}
+        {/*</View>*/}
+        {/*<Text style={{ color: 'black', textAlign: 'center' }}>COPYRIGHT © {webName} RESERVED</Text>*/}
+        {/*<Text style={{ color: '#000000', textAlign: 'center' }}>{'VERSION : 13'}</Text>*/}
         <View style={{ height: 100 }} />
       </ScrollView>
       <Activitys uid={uid} isTest={isTest} refreshing={refreshing} redBagLogo={redBagLogo} redBag={redBag}
@@ -218,85 +206,6 @@ const LCHomePage = ({ setProps }) => {
   )
 }
 
-const TurntableListItem = () => {
-  const { width, height } = useDimensions().screen
-  const { isTest = false, uid = '' } = UGStore.globalProps.userInfo
-  const [turntableListVisiable, setTurntableListVisiable] = useState(false)
-  const [turntableList, setTurntableList] = useState<TurntableListModel>()
-  useEffect(() => {
-    if (turntableList) {
-      setTurntableListVisiable(true)
-    }
-  }, [turntableList])
-  const getTurntableList = async () => {
-    try {
-      const { data, status } = await APIRouter.activity_turntableList()
-      setTurntableList(data.data)
-    } catch (error) {
-
-    }
-  }
-  useEffect(() => {
-    if (uid != '') {
-      getTurntableList()
-    }
-  }, [uid])
-  if (turntableListVisiable) {
-    return (
-      <TouchableWithoutFeedback onPress={() => {
-        if (uid == '') {
-          Alert.alert('温馨提示', '您还未登录', [
-            {
-              text: '取消', onPress: () => {
-              }, style: 'cancel',
-            },
-            {
-              text: '马上登录', onPress: () => {
-                navigate(PageName.ZLLoginPage, {})
-              },
-            },
-          ])
-        } else if (isTest) {
-          Alert.alert('温馨提示', '请先登录您的正式帐号', [
-            {
-              text: '取消', onPress: () => {
-              }, style: 'cancel',
-            },
-            {
-              text: '马上登录', onPress: () => PushHelper.pushLogin(),
-            },
-          ])
-        } else {
-          if (Platform.OS != 'ios') return
-          const turntableListModel = Object.assign({ clsName: 'DZPModel' }, turntableList?.[0])
-          OCHelper.call(({ vc }) => ({
-            vc: {
-              selectors: 'DZPMainView.alloc.initWithFrame:[setItem:]',
-              args1: [NSValue.CGRectMake(100, 100, AppDefine.width - 60, AppDefine.height - 60)],
-              args2: [turntableListModel],
-            },
-            ret: {
-              selectors: 'SGBrowserView.showMoveView:yDistance:',
-              args1: [vc, 100],
-            },
-          }))
-        }
-      }}>
-        <ImageBackground style={{ width: 95, height: 95, position: 'absolute', top: height / 2, right: 20 }}
-                         source={{ uri: 'dzp_btn' }}>
-          <TouchableWithoutFeedback onPress={() => {
-            setTurntableListVisiable(false)
-          }}>
-            <Image style={{ width: 20, height: 20, right: 0, top: 0, position: 'absolute' }}
-                   source={{ uri: 'dialog_close' }} />
-          </TouchableWithoutFeedback>
-        </ImageBackground>
-      </TouchableWithoutFeedback>)
-  } else {
-    return null
-  }
-
-}
 const Banner = ({ bannerData, onlineNum = 0, showOnlineCount = true, customHeight, interval }: { bannerData: List[], onlineNum: number, showOnlineCount?: boolean, customHeight?: number, interval?: any }) => {
   const { width } = useDimensions().window
   const BannerRef = React.useRef<Carousel>()
