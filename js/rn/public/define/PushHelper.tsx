90a46bef45eaa943c178ad0afeeaabe124587d53
Ezer <ugrnezer@gmail.com>
2021-01-15 17:53:58 +0800

    add Android OnlineService

--- js/rn/public/define/PushHelper.tsx
+++ js/rn/public/define/PushHelper.tsx
@@ -140,7 +140,7 @@ export default class PushHelper {
 
         if (game?.seriesId == 7 && game?.subId == GameType.游戏大厅) {  //游戏大厅
           push(PageName.GameLobbyPage, { showBackButton: true })
-          return 
+          return
         }
 
         ANHelper.callAsync(CMD.OPEN_NAVI_PAGE, game)
@@ -576,8 +576,8 @@ export default class PushHelper {
             return
           }
           case UGUserCenterType.在线客服: {
-            subId = MenuType.KF
-            break
+            push(PageName.OnlineService)
+            return
           }
           case UGUserCenterType.活动彩金: {
             subId = MenuType.SQCJ
