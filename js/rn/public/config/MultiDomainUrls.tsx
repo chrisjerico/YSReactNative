0a1aa2157579f8b5984c0ac1a7c35f49be8ef1d0
Arc <arclinde@gmail.com>
2021-01-16 15:42:22 +0800

    多余的域名

--- js/rn/public/config/MultiDomainUrls.tsx
+++ js/rn/public/config/MultiDomainUrls.tsx
@@ -348,7 +348,6 @@ const MultiDomainUrls: { [x: string]: Array<string> } = {
   ],
   'c213': [
     'https://c213-app-450.com',
-    'https://450-xpj-app.com',
     'https://app-450-xpj.co',
     'https://c213-app-450.cc',
   ],
