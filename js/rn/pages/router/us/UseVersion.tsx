a4ca0b1403ad58b50ca48e67f3422fe89486d1f3
Arc <arclinde@gmail.com>
2021-01-16 22:17:09 +0800

    域名更新检测判断

--- js/rn/pages/router/us/UseVersion.tsx
+++ js/rn/pages/router/us/UseVersion.tsx
@@ -54,8 +54,11 @@ const UseVersion = ({
       })
   }
 
-  //查找最快的域名
-  const testSite = async (finish?: (succ: boolean) => void) => {
+  /**
+   * 查找最快的域名
+   * @param callback 是否有正常的域名
+   */
+  const testSite = async (callback: (result?: boolean) => void) => {
     // 站点编号
     let siteId = ''
     switch (Platform.OS) {
@@ -69,6 +72,9 @@ const UseVersion = ({
 
     //ugLog('site = siteId', siteId)
     let domains = MultiDomainUrls[siteId]
+
+    //域名多于2条才处理
+    // if (anyLength(domains) < 2) return
     //ugLog('site = domains 7 ', domains)
 
     let firstUrl = '' //哪条速度最快用哪条
@@ -85,10 +91,10 @@ const UseVersion = ({
           //ugLog('site = response 7 ', url, res?.data)
           //最快的那一条
           if (res?.status === 200 && res?.data?.code === 0 && anyEmpty(firstUrl)) {
+            callback && callback(true)
             firstUrl = url
             recombineDomain({ [siteId]: firstUrl })
             notifyDomainChanged(siteId)
-            finish && finish(true)
             //ugLog('site = firstUrl 6', url)
           }
           return res
