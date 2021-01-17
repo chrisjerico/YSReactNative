3a09d5cfa1cd331719d10709759b615da74f4b38
smith <kangshiming123@gmail.com>
2021-01-17 20:34:48 +0800

    c208修改域名

--- js/rn/public/config/MultiDomainUrls.tsx
+++ js/rn/public/config/MultiDomainUrls.tsx
@@ -334,7 +334,7 @@ const MultiDomainUrls: { [x: string]: Array<string> } = {
     'http://rdiuyotdhjo533fsddylvgi.com',
   ],
   'c208': [
-    'https://771771z.com',
+    'https://771bb771.com',
     'https://771771s.com',
     'https://771771i.com',
     'https://771771j.com',
