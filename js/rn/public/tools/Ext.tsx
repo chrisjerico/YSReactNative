37f316d936baa6ab388896ff7c5b5ee29231dca5
Arc <arclinde@gmail.com>
2021-01-30 14:27:56 +0800

    调整判断

--- js/rn/public/tools/Ext.tsx
+++ js/rn/public/tools/Ext.tsx
@@ -22,7 +22,7 @@ export const arrayLength = (value?: Array<any>) => arrayEmpty(value) ? 0 : value
 export const anyNull = (value?: any) => value == null || value == 'undefined'
 export const anyEmpty = (value?: any) => anyNull(value) || value === '' || value.length <= 0
 export const anyLength = (value?: any) => anyEmpty(value) ? 0 : value.length
-  export const dicNull = (value?: any) => JSON.stringify(value) == '{}';
+export const dicNull = (value?: any) => anyNull(value) || JSON.stringify(value) == '{}';
 
 const isObject = (obj) => Object.prototype.toString.call(obj) === '[object Object]'
 const isArray = (arr) => Array.isArray(arr)
