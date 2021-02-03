52ad5b7bd2ede774249f579087712190e7563d3a
Arc <arclinde@gmail.com>
2021-02-03 20:58:49 +0800

    二字定位下注

--- js/rn/pages/bet/board/tools/ezdw/BetEZDWUtil.tsx
+++ js/rn/pages/bet/board/tools/ezdw/BetEZDWUtil.tsx
@@ -1,15 +1,10 @@
 import * as React from 'react'
-import { PlayData, PlayGroupData, PlayOddData } from '../../../../../public/network/Model/lottery/PlayOddDetailModel'
+import { PlayData, PlayOddData } from '../../../../../public/network/Model/lottery/PlayOddDetailModel'
 import { arrayLength } from '../../../../../public/tools/Ext'
 import { SelectedPlayModel } from '../../../../../redux/model/game/SelectedLotteryModel'
-import { CqsscCode, LhcCode } from '../../../const/LotteryConst'
-import { UGStore } from '../../../../../redux/store/UGStore'
+import { CqsscCode } from '../../../const/LotteryConst'
 import { gatherSelectedItems } from '../BetUtil'
-import { numberToFloatString } from '../../../../../public/tools/StringUtil'
-import { BetLotteryData } from '../../../../../public/network/it/bet/IBetLotteryParams'
-import { zodiacPlayX } from '../hx/BetHXUtil'
 import { ugLog } from '../../../../../public/tools/UgLog'
-import { playDataX } from '../zxbz/BetZXBZUtil'
 /**
  * X字定位下注工具类
  */
@@ -40,20 +35,38 @@ const combineEZDWArray = (...selData: Array<SelectedPlayModel>): Array<string> =
  * 并且剔除多余的数据将结果返回
  * @param selData
  */
-const combineEZDWModel = (...selData: Array<SelectedPlayModel>): Array<string> => {
-  // return selData1?.plays.map((item1) => selData2?.plays.map((item2) => `${item1?.name},${item2?.name}`))?.flat(Infinity)
-  if (arrayLength(selData) == 1) {
-    return selData[0].plays?.map((play) => play?.name)
-  } else if (arrayLength(selData) == 2) {
-    return selData[0]?.plays?.map((play0) =>
-      selData[1]?.plays?.map((play1) =>
-        `${play0.name},${play1.name}`)).flat(2)
-  } else if (arrayLength(selData) == 3) {
-    return selData[0]?.plays?.map((play0) =>
-      selData[1]?.plays?.map((play1) =>
-        selData[2]?.plays?.map((play2) =>
-          `${play0.name},${play1.name},${play2.name}`))).flat(3)
+const combineEZDWModel = (selData: Array<SelectedPlayModel>): SelectedPlayModel => {
+  if (arrayLength(selData) > 0) {
+    return {
+      ...selData[0], plays: combineEZDWPlayData(selData),
+    } as SelectedPlayModel
   }
+
+  return null
+}
+
+/**
+ * 把多页数据组合成 一维数组
+ * @param selData
+ * @param plays 追加的彩球数据
+ */
+const combineEZDWPlayData = (selData?: Array<SelectedPlayModel>, plays?: Array<PlayData>): Array<PlayData> => {
+  if (arrayLength(selData) > 1) {
+    const arr0 = selData.slice(0, 1) as Array<SelectedPlayModel>
+    const arr1 = selData.slice(1) as Array<SelectedPlayModel>
+    ugLog('arr0 = ', JSON.stringify(arr0))
+    ugLog('arr1 = ', JSON.stringify(arr1))
+    return arr0[0]?.plays.map((play) => combineEZDWPlayData(arr1, [...plays, play])).flat(Infinity) as Array<PlayData>
+  } else if (arrayLength(selData) == 1) {
+    ugLog('selData arr = 1 = ', JSON.stringify(selData))
+    ugLog('selData plays = 1 = ', JSON.stringify(plays))
+    return selData[0]?.plays?.map((play) =>
+      ({
+        ...play,
+        name: [...plays, play].map((item) => item?.name).toString(),
+      } as PlayData))
+  }
+
   return null
 }
 
@@ -67,18 +80,22 @@ const combineSelectedData = (currentPlayOddData?: PlayOddData,
   const newSelectedData = JSON.parse(JSON.stringify(selectedData))
   return Object.keys(newSelectedData).map((key) => {
     switch (key) {
-      case CqsscCode.EZDW: //二字定位，2组数据合成1组数据
-        const data1 = (newSelectedData[key][0][0] as SelectedPlayModel)
-        const data2 = (newSelectedData[key][0][1] as SelectedPlayModel)
-
-        break
+      case CqsscCode.EZDW: //二字定位，多组数据合成1组数据
+        ugLog('key data - = ', JSON.stringify(Object.values(newSelectedData[key])))
+        ugLog('key data 2 - = ', JSON.stringify(Object.values(newSelectedData[key]).map((data) => Object.values(data))))
+        const ezData = (Object.values(newSelectedData[key]).map((data) => Object.values(data)).flat(Infinity) as Array<SelectedPlayModel>)
+        ugLog('ezData - = ', JSON.stringify(ezData))
+        const arr = combineEZDWModel(ezData)
+        ugLog('arr - = ', JSON.stringify(arr))
+        return arr
       default:
         return gatherSelectedItems(key, newSelectedData)
     }
-  }).flat(2)
+  }).flat(Infinity) as Array<SelectedPlayModel>
 }
 
 export {
   combineEZDWArray,
   combineSelectedData,
+  combineEZDWModel,
 }
--- js/rn/pages/bet/util/selecte/ParseLMASelectedUtil.tsx
+++ js/rn/pages/bet/util/selecte/ParseLMASelectedUtil.tsx
@@ -15,7 +15,7 @@ const parseLMASelectedData = (playOddData: PlayOddData, selectedBalls: Array<str
   //选中了哪些球, 3层结构
   const selGroup = new Map<string, Map<string, SelectedPlayModel>>()//重新组合的新数据如 特码TM -> 对应的数据
 
-  // ugLog(' playOddData ======== ', JSON.stringify(playOddData))
+  ugLog(' playOddData ======== ', JSON.stringify(playOddData))
   //遍历每一TAB的数据 如 特码B和特码A
   playOddData?.pageData?.groupTri?.map((pageData, index) => {
     const tabMap = new Map<string, SelectedPlayModel>() //每一个TAB的数组 如 特码B TMB -> 对应的数据
@@ -24,16 +24,19 @@ const parseLMASelectedData = (playOddData: PlayOddData, selectedBalls: Array<str
     ugLog('tabMap code index = ', pageData[0].alias, index)
 
     //遍历TAB的每一组数据，如特码B里面有 特码数据，两面数据，色波数据
-    pageData?.map((groupData) => {
+    pageData?.map((groupData, index) => {
 
       //找出选中的球对应的原始数据, 优先使用 自定义数组 exPlays
       const selBalls = !anyEmpty(groupData?.exPlays) ?
         groupData?.exPlays?.filter((item) => selectedBalls.includes(item?.exId ?? item?.id)) :
         groupData?.plays?.filter((item) => selectedBalls.includes(item?.exId ?? item?.id))
 
+      const pageAlias = `${groupData?.alias},${index}`
+      ugLog('pageAlias = ', pageAlias)
+
       //再用原始数组和彩种数据组合成 新的选中数据
       !anyEmpty(selBalls) && (
-        tabMap[groupData?.alias] = {
+        tabMap[pageAlias] = {
           playGroups: groupData,
           plays: selBalls
         } as SelectedPlayModel //key=取第一组数据的ID作为Tab标识, value=每一组数据，如 特码B里面的 两面, 色波
