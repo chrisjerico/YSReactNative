2fb8327a891536ce08bdf179a91f3bd495ad43eb
fish <gold.fish.a1b1@gmail.com>
2021-02-07 14:55:26 +0800

    138738】c193】IOS】原生APP域名 4906212.com 换成 4906214.com 【普通】

--- js/rn/public/config/MultiDomainUrls.tsx
+++ js/rn/public/config/MultiDomainUrls.tsx
@@ -327,6 +327,7 @@ const MultiDomainUrls: { [x: string]: Array<string> } = {
     'https://474745.com',
   ],
   'c193': [
+    'https://4906214.com',
     'https://49060333.com',
     'https://49060222.com',
     'https://49061111.com',
