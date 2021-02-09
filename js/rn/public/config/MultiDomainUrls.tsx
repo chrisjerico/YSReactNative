1cad5b89bd305eb3d40b972d4cf4a95007d97051
Arc <arclinde@gmail.com>
2021-02-09 21:00:51 +0800

    t126变 t136

--- js/rn/public/config/MultiDomainUrls.tsx
+++ js/rn/public/config/MultiDomainUrls.tsx
@@ -120,7 +120,7 @@ const MultiDomainUrls: { [x: string]: Array<string> } = {
     'http://t502f.fhptcdn.com',
   ],
   'txtTest': [
-    'http://t136f.fhptchat01.com',
+    'http://t136f.fhptcdn.com',
   ],
   'tstksm': [
     'http://t133f.6yc.com',
