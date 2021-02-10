d6db7ff9b2a69e33d28fb0765b72aae07675cb84
fish <gold.fish.a1b1@gmail.com>
2021-02-10 16:07:57 +0800

    1

--- js/rn/public/network/NetworkRequest1/model/activity/index.ts
+++
@@ -1,8 +0,0 @@
-
-interface Settings {
-  goldenEggLogo: string,//砸金蛋图标
-  redBagLogo: string,//红包图标
-  redBagSkin: string,//红包皮肤
-  scratchOffLogo: string,//刮刮乐图标
-  turntableLogo: string,//大转盘图标
-}
