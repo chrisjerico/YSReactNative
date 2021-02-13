d5b1140ad00daf6137d9415f60c2c079c7fe4e30
Arc <arclinde@gmail.com>
2021-02-13 14:44:03 +0800

    测试服

--- js/rn/public/config/MultiDomainUrls.tsx
+++ js/rn/public/config/MultiDomainUrls.tsx
@@ -99,7 +99,7 @@ const MultiDomainUrls: { [x: string]: Array<string> } = {
     'http://test07.6yc.com',
   ],
   'txt126': [
-    'http://t136f.6yc.com',
+    'http://t126f.6yc.com',
   ],
   'txt127': [
     'http://t127f.fhptcdn.com',
@@ -120,7 +120,7 @@ const MultiDomainUrls: { [x: string]: Array<string> } = {
     'http://t502f.fhptcdn.com',
   ],
   'txtTest': [
-    'http://t136f.6yc.com',
+    'http://t126f.6yc.com',
   ],
   'tstksm': [
     'http://t133f.6yc.com',
