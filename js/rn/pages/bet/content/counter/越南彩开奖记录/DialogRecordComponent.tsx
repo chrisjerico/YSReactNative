67e550309f054c093e0d6608629e65783857c545
Arc <arclinde@gmail.com>
2021-02-26 19:17:28 +0800

    越南彩开奖记录颜色

--- js/rn/pages/bet/content/counter/越南彩开奖记录/DialogRecordComponent.tsx
+++ js/rn/pages/bet/content/counter/越南彩开奖记录/DialogRecordComponent.tsx
@@ -15,6 +15,9 @@ import CommStyles from '../../../../base/CommStyles'
 import { NextIssueData } from '../../../../../public/network/Model/lottery/NextIssueModel'
 import { AnimZoomInOut, AnimZoomOutIn } from '../../../anim/BetAnim'
 import NormalDialogComponent, { INormalDialogButton } from '../../../../../public/widget/dialog/普通对话框/NormalDialogComponent'
+import { currentPlayOddData, currentTabGroupData } from '../../../util/select/ParseSelectedUtil'
+import { HoChiMin, HoChiMinSub, LCode } from '../../../const/LotteryConst'
+import { anyLength, arrayLength } from '../../../../../public/tools/Ext'
 
 interface IDialogRecordComponent {
   nextIssueData?: NextIssueData //下一期数据
@@ -53,14 +56,14 @@ const DialogRecordComponent = ({
       '无头': '尾巴',
       '无0': nextIssueData?.t0,
       '无1': nextIssueData?.t1,
-      '无2': '',
-      '无3': nextIssueData?.t2,
-      '无4': nextIssueData?.t3,
-      '无5': '',
-      '无6': nextIssueData?.t4,
-      '无7': nextIssueData?.t5,
-      '无8': '',
-      '无9': nextIssueData?.t7,
+      '无2': nextIssueData?.t2,
+      '无3': nextIssueData?.t3,
+      '无4': nextIssueData?.t4,
+      '无5': nextIssueData?.t5,
+      '无6': nextIssueData?.t6,
+      '无7': nextIssueData?.t7,
+      '无8': nextIssueData?.t8,
+      '无9': nextIssueData?.t9,
     }
   }, [nextIssueData])
 
@@ -75,18 +78,125 @@ const DialogRecordComponent = ({
     !windowShow && onClosingDialog && onClosingDialog()
   }, [windowShow])
 
+  /**
+   * 绘制彩色文字
+   * @param text
+   * @param textIndex
+   * @param array
+   */
+  const renderMulColorText = (text?: string, textIndex?: number, array?: string[]) => {
+    const gameCode = currentPlayOddData()?.code
+    const tabCode = currentTabGroupData()[0]?.plays[0]?.code
+
+    let textArray = text?.split(',')
+
+    const viewArr = textArray?.map((item, index) => {
+
+      //红色字体起始位置，一般是 有几页，就有几位的红色
+      let colorStart = anyLength(item) - arrayLength(currentTabGroupData())
+      //红色截止位置，把一个字符串分成3段，如 12345 -> 123 4 5
+      let colorEnd = item.length
+
+      switch (true) {
+        case tabCode == HoChiMinSub.BIAOTI: //只有最后一个红色
+          if (textIndex != arrayLength(array) - 1) {
+            colorStart = anyLength(item)
+          }
+          break
+
+        case tabCode == HoChiMinSub.ZHUANTI: //只有第一个红色
+          if (textIndex != 0) {
+            colorStart = anyLength(item)
+          }
+          break
+
+        case tabCode == HoChiMinSub.BIAOTIWB: // 只有第一个, 最后一个红色
+          if (textIndex != arrayLength(array) - 1 && textIndex != 0) {
+            colorStart = anyLength(item)
+          }
+          break
+
+        case tabCode == HoChiMinSub.TOU: // 只有第一个数字是红色
+          colorStart = item.length - 2
+          colorEnd = item.length - 1
+          if (textIndex != 0) {
+            colorStart = anyLength(item)
+          }
+          break
+
+        case tabCode == HoChiMinSub.WEI: // 只有第一个数字是红色
+          colorStart = item.length - 1
+          colorEnd = item.length
+          if (textIndex != 0) {
+            colorStart = anyLength(item)
+          }
+          break
+
+        case tabCode == HoChiMinSub.H_3YINJIE: // 只有第7个数字是红色
+          if (textIndex == 7) {
+            colorStart = 0
+          } else {
+            colorStart = anyLength(item)
+          }
+          break
+
+        case tabCode == HoChiMinSub.H_4GTEBIE: // 只有第1个数字是红色
+          if (textIndex == 0) {
+            colorStart = item.length - 4
+          } else {
+            colorStart = anyLength(item)
+          }
+          break
+
+        // case gameCode == HoChiMin.DDQX:
+        //
+        //   break
+
+        default:
+          // colorStart = 2
+          break
+
+      }
+
+      let textArr: string[]
+      if (anyLength(item) >= colorStart) {
+        textArr = [
+          item?.slice(0, colorStart),
+          item?.slice(colorStart, colorEnd),
+          item?.slice(colorEnd, item.length),
+        ]
+      } else {
+        textArr = [item, '', '']
+      }
+
+      return (
+        <Text style={_styles.item_text}>
+          {textArr[0]}
+          <Text style={_styles.item_text_2}>{
+            textArr[1]
+          }
+          </Text>
+          {textArr[2]}
+          {index < arrayLength(textArray) - 1 ? '-' : ''}
+        </Text>
+      )
+    })
+
+    return (<View style={{ flex: 1, flexDirection: 'row', flexWrap: 'wrap' }}>{viewArr}</View>)
+  }
+
   //左边条目
   const renderLeftColumn = () => {
     return (
       <View style={_styles.content_left}>
         {
-          Object.keys(leftData)?.map((key, index) => (
+          Object.keys(leftData)?.map((key, index, array) => (
             <View style={[
               _styles.content_left_container,
-              index === 4 ? {height: 3 * ITEM_HEIGHT} : null,
+              index === 4 ? { height: 3 * ITEM_HEIGHT } : null,
             ]}>
               <Text style={_styles.item_text_title}>{key}</Text>
-              <Text style={_styles.item_text}>{leftData[key]}</Text>
+              {renderMulColorText(leftData[key], index, array)}
             </View>
           ))
         }
@@ -150,6 +260,7 @@ const _styles = StyleSheet.create({
     height: ITEM_HEIGHT,
     borderWidth: scale(0.5),
     borderColor: UGColor.LineColor4,
+    paddingRight: scale(4),
   },
   content_right: {
     flex: 1,
@@ -157,13 +268,16 @@ const _styles = StyleSheet.create({
   item_text_title: {
     color: UGColor.TextColor3,
     fontSize: scale(22),
-    paddingHorizontal: scale(16)
+    paddingHorizontal: scale(16),
+    minWidth: scale(56),
   },
   item_text: {
     color: UGColor.TextColor3,
     fontSize: scale(20),
-    flex: 1,
-    paddingRight: scale(4)
+  },
+  item_text_2: {
+    color: UGColor.RedColor5,
+    fontSize: scale(20),
   },
   // mmc_image: {
   //   width: '100%',
