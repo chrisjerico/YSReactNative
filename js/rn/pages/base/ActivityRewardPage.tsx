c7f021135767e99535ccd8ea0cf195d6839c2a43
andrew <andrew@gmail.com>
2021-02-27 14:15:20 +0800

    完成弹框

--- js/rn/pages/base/ActivityRewardPage.tsx
+++ js/rn/pages/base/ActivityRewardPage.tsx
@@ -18,6 +18,8 @@ import { ugLog } from '../../public/tools/UgLog'
 import { api } from '../../public/network/NetworkRequest1/NetworkRequest1'
 import { scale } from '../../public/tools/Scale'
 
+
+
 interface ApplyRewardProps {
   tabLabel: string
   titleArray: any[]
@@ -25,6 +27,7 @@ interface ApplyRewardProps {
   onPress: () => any
   onPressApply: ({ win_apply_content }: { win_apply_content: string }) => any
 }
+
 
 const RewardList = ({ data, uniqueKey, onPress, onPressApply }) => (
   <List
@@ -70,9 +73,9 @@ function dataAction(category: string, list: any[],) {
 const ApplyReward = ({ tabLabel, titleArray, list, onPress, onPressApply }: ApplyRewardProps) => {
   return (
     <View style={{ flex: 1 }}>
-      <ScrollableTabView 
-      tabBarUnderlineStyle={
-        { backgroundColor: Skin1.themeColor , height: scale(3),}}
+      <ScrollableTabView
+        tabBarUnderlineStyle={
+          { backgroundColor: Skin1.themeColor, height: scale(3), }}
         tabBarActiveTextColor={Skin1.textColor1}
         tabBarInactiveTextColor={Skin1.tabNoSelectColor}
         tabBarTextStyle={{ fontSize: scale(20) }}
@@ -92,71 +95,6 @@ const ApplyReward = ({ tabLabel, titleArray, list, onPress, onPressApply }: Appl
   )
 }
 
-// cell 点击方法
-function itemAction(item: any) {
-  // applyWinLogDetail
- 
-  api.activity.applyWinLogDetail(item.id).useSuccess((data) => {
-   ugLog('data==',data)
-   let  objdata = data['data'];
-   
-   let str :string = `
-   活动名称：${objdata.winName}
-   申请日期：${objdata.updateTime}
-   申请金额：${objdata.amount}
-   申请原因：${objdata.userComment} 
-   审核结果：${objdata.state} 
-   审核说明：${objdata.adminComment}
-   `
-
-   let str1 = '活动名称：'+objdata.winName+'\n'+'申请日期：'+objdata.updateTime+'\n'
-   Alert.alert('查看详情', str1, [
-     {
-       text: '关闭',
-       onPress: () => {
-       },
-       style: 'cancel',
-     },
-     
-   ])
-
-  })
- 
-
-
-
-}
-
-const ApplyFeedBack = ({ tabLabel, list }) => {
-  ugLog('list===',list)
-  return (
-    <List
-      uniqueKey={'ApplyFeedBack'}
-      ListHeaderComponent={() => (
-        <View style={{ flexDirection: 'row', justifyContent: 'space-around', borderBottomColor: '#d9d9d9', borderBottomWidth: AppDefine.onePx, paddingVertical: 10 }}>
-          <UGText style={{ fontWeight: '500' ,}}>{'         申请日期          '}</UGText>
-          <UGText style={{ fontWeight: '500', }}>{'申请金额'}</UGText>
-          <UGText style={{ fontWeight: '500' }}>{'状态'}</UGText>
-        </View>
-      )}
-      data={list}
-      scrollEnabled={true}
-      keyExtractor={(item, index) => index.toString()}
-      renderItem={({ item }) => {
-        return (
-        <TouchableOpacity style={{ flexDirection: 'row', justifyContent: 'space-around', borderBottomColor: '#d9d9d9', borderBottomWidth: AppDefine.onePx, paddingVertical: 10 ,}}
-        onPress={() => {
-          itemAction(item)
-        }}>
-        <UGText style={{ fontWeight: '500', }}>{item.updateTime}</UGText>
-        <UGText style={{ fontWeight: '500' ,}}>{item.amount}</UGText>
-        <UGText style={{ fontWeight: '500' }}>{item.state}</UGText>
-      </TouchableOpacity>
-        );
-      }}
-    />
-  )
-}
 
 const ActivityRewardPage = () => {
   const [loading, setLoading] = useState(true)
@@ -167,8 +105,59 @@ const ActivityRewardPage = () => {
   const [activityVisible, setActivityVisible] = useState(false)
   const [applyVisible, setApplyVisible] = useState(false)
   const [activityContent, setActivityContent] = useState('')
+  const [alertVisible, setAlertVisible] = useState(false)
+  const [content, setContent] = useState('')
 
+  // cell 点击方法
+  function itemAction(item: any) {
+    // applyWinLogDetail
+    api.activity.applyWinLogDetail(item.id).useSuccess((data) => {
 
+      let objdata = data['data'];
+
+      let str: string = `
+活动名称：${objdata.winName}
+申请日期：${objdata.updateTime}
+申请金额：${objdata.amount}
+申请原因：${objdata.userComment} 
+审核结果：${objdata.state} 
+审核说明：${objdata.adminComment}
+   `
+      setContent(str)
+      setAlertVisible(true)
+    })
+  }
+
+  const ApplyFeedBack = ({ tabLabel, list }) => {
+    ugLog('list===', list)
+    return (
+      <List
+        uniqueKey={'ApplyFeedBack'}
+        ListHeaderComponent={() => (
+          <View style={{ flexDirection: 'row', justifyContent: 'space-around', borderBottomColor: '#d9d9d9', borderBottomWidth: AppDefine.onePx, paddingVertical: 10 }}>
+            <UGText style={{ fontWeight: '500', }}>{'         申请日期          '}</UGText>
+            <UGText style={{ fontWeight: '500', }}>{'申请金额'}</UGText>
+            <UGText style={{ fontWeight: '500' }}>{'状态'}</UGText>
+          </View>
+        )}
+        data={list}
+        scrollEnabled={true}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity style={{ flexDirection: 'row', justifyContent: 'space-around', borderBottomColor: '#d9d9d9', borderBottomWidth: AppDefine.onePx, paddingVertical: 10, }}
+              onPress={() => {
+                itemAction(item)
+              }}>
+              <UGText style={{ fontWeight: '500', }}>{item.updateTime}</UGText>
+              <UGText style={{ fontWeight: '500', }}>{item.amount}</UGText>
+              <UGText style={{ fontWeight: '500' }}>{item.state}</UGText>
+            </TouchableOpacity>
+          );
+        }}
+      />
+    )
+  }
   // 删除未分类
   function filterByName(aim: [], category: string) {
     return aim.filter(item => item.category !== category)
@@ -214,7 +203,7 @@ const ActivityRewardPage = () => {
         //@ts-ignore
         const applyWinLog = value[1]?.data?.data?.list
 
-        ugLog('applyWinLog====',applyWinLog)
+        ugLog('applyWinLog====', applyWinLog)
         setWinApplyList(winApplyList)
         setApplyWinLog(applyWinLog)
       })
@@ -247,7 +236,7 @@ const ActivityRewardPage = () => {
           <ProgressCircle />
         ) : (
             <ScrollableTabViewComponent
-             indicatorStyle={{ width: 50 }} tabBarScrollEnabled={false}
+              indicatorStyle={{ width: 50 }} tabBarScrollEnabled={false}
             >
               <ApplyReward
                 tabLabel={'申请彩金'}
@@ -260,14 +249,14 @@ const ActivityRewardPage = () => {
                   setApplyVisible(true)
                   setActivityContent(win_apply_content)
                 }}
-                
+
               />
               <ApplyFeedBack tabLabel={headerTitle()} list={applyWinLog} />
             </ScrollableTabViewComponent>
           )}
       </View>
       <Modal transparent={true} style={{ flex: 1, backgroundColor: 'transparent' }} visible={activityVisible}>
-        <View style={{ backgroundColor: '#0005', flex: 1, justifyContent: 'center', alignItems: 'center' ,borderColor: '#d9d9d9',borderWidth:AppDefine.onePx,}}>
+        <View style={{ backgroundColor: '#0005', flex: 1, justifyContent: 'center', alignItems: 'center', borderColor: '#d9d9d9', borderWidth: AppDefine.onePx, }}>
           <View style={{ width: '75%', height: 200, backgroundColor: '#ffffff', borderRadius: 10, alignItems: 'center' }}>
             <UGText style={{ fontSize: 18, fontWeight: '500', marginVertical: 10 }}>{'彩金活动'}</UGText>
             <Image
@@ -327,6 +316,27 @@ const ActivityRewardPage = () => {
             </View>
           </View>
         </View>
+      </Modal>
+      {/* 申请反馈弹框 */}
+      <Modal transparent={true} style={{ flex: 1, backgroundColor: 'transparent' }} visible={alertVisible}>
+      <View style={{ backgroundColor: '#0005', flex: 1,width:'100%', justifyContent: 'center', alignItems: 'center', }}>
+        <View style={{ width: '75%', backgroundColor: '#ffffff', borderRadius: 10, alignItems: 'center' }}>
+          <UGText style={{ fontSize: 18, fontWeight: '500', marginVertical: 10 }}>{'查看详情'}</UGText>
+          <View style={{ flexDirection: 'row', }}>
+            <UGText style={{ marginHorizontal: 15, }}>{content}</UGText>
+          </View>
+          <View style={{ flexDirection: 'row', width: '100%', height: 50, borderTopWidth: AppDefine.onePx, borderColor: '#d9d9d9' }}>
+            <Button
+              title={'关闭'}
+              containerStyle={{ flex: 1 }}
+              titleStyle={{ color: '#2894FF', fontSize: 18 }}
+              onPress={() => {
+                setAlertVisible(false)
+              }}
+            />
+          </View>
+        </View>
+      </View>
       </Modal>
     </>
   )
